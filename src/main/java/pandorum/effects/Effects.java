e0a5374d37915e3f85f09336003ee8023806ac85
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-06 19:27:12 +0300

    Fix

--- src/main/java/pandorum/effects/Effects.java
+++ src/main/java/pandorum/effects/Effects.java
@@ -4,9 +4,9 @@ import mindustry.gen.*;
 
 public class Effects {
 
-    private static final EffectObject moveEffect = new EffectData(0, 0, 30, "#4169e1ff", "freezing");
-    private static final EffectObject leaveEffect = new EffectData(0, 0, 30, "#4169e1ff", "greenLaserCharge");
-    private static final EffectObject joinEffect = new EffectData(0, 0, 30, "#4169e1ff", "greenBomb");
+    private static final EffectObject moveEffect = new EffectObject(0, 0, 30, "#4169e1ff", "freezing");
+    private static final EffectObject leaveEffect = new EffectObject(0, 0, 30, "#4169e1ff", "greenLaserCharge");
+    private static final EffectObject joinEffect = new EffectObject(0, 0, 30, "#4169e1ff", "greenBomb");
 
     public static void onMove(Player p) {
         moveEffect.spawn(p.x(), p.y());
