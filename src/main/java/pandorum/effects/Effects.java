152f11b03e85dfad81c0534fe3c3c1d90cfde5aa
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-06 19:22:38 +0300

    Create Effects.java

---
+++ src/main/java/pandorum/effects/Effects.java
@@ -0,0 +1,22 @@
+package pandorum.effects;
+
+import mindustry.gen.*;
+
+public class Effects {
+
+    private static final EffectObject moveEffect = new EffectData(0, 0, 30, "#4169e1ff", "freezing");
+    private static final EffectObject leaveEffect = new EffectData(0, 0, 30, "#4169e1ff", "greenLaserCharge");
+    private static final EffectObject joinEffect = new EffectData(0, 0, 30, "#4169e1ff", "greenBomb");
+
+    public static void onMove(Player p) {
+        moveEffect.spawn(p.x(), p.y());
+    }
+
+    public static void onJoin(Player p) {        
+        joinEffect.spawn(p.x(), p.y());
+    }
+
+    public static void onLeave(Player p) {
+         leaveEffect.spawn(p.x(), p.y());
+    }
+}
