3d66e9ee0aedca0b5e574e46c07258f3efc7eba1
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-06 19:40:09 +0300

    Add nullCheck

--- src/main/java/pandorum/effects/Effects.java
+++ src/main/java/pandorum/effects/Effects.java
@@ -9,14 +9,17 @@ public class Effects {
     private static final EffectObject joinEffect = new EffectObject(0, 0, 30, "#4169e1ff", "greenBomb");
 
     public static void onMove(Player p) {
-        moveEffect.spawn(p.x(), p.y());
+        try { moveEffect.spawn(p.x, p.y); }
+        catch (NullPointerException e) {}
     }
 
     public static void onJoin(Player p) {        
-        joinEffect.spawn(p.x(), p.y());
+        try { moveEffect.spawn(p.x, p.y); }
+        catch (NullPointerException e) {}
     }
 
     public static void onLeave(Player p) {
-         leaveEffect.spawn(p.x(), p.y());
+        try { moveEffect.spawn(p.x, p.y); }
+        catch (NullPointerException e) {}
     }
 }
