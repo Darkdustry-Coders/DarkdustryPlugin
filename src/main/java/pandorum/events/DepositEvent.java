74a72458d47ee5fb12b12d74b19516ee35d5354f
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-07 15:54:50 +0300

    Update DepositEvent.java

--- src/main/java/pandorum/events/DepositEvent.java
+++ src/main/java/pandorum/events/DepositEvent.java
@@ -8,13 +8,13 @@ import mindustry.content.Items;
 
 import pandorum.PandorumPlugin;
 import pandorum.comp.Config.PluginType;
-import static pandorum.Misc.bundled;
+import static pandorum.Misc.*;
 
 public class DepositEvent {
     public static void call(final EventType.DepositEvent event) {
         if(PandorumPlugin.config.type == PluginType.other) return;
-        if(event.tile.block() == Blocks.thoriumReactor && event.item == Items.thorium && target.team().cores().contains(c -> event.tile.dst(c.x, c.y) < PandorumPlugin.config.alertDistance)){
-            Groups.player.each(p -> !alertIgnores.contains(p.uuid()), p -> bundled(p, "events.withdraw-thorium", Misc.colorizedName(event.player), event.tile.tileX(), event.tile.tileY()));
+        if(event.tile.block() == Blocks.thoriumReactor && event.item == Items.thorium && event.player.team().cores().contains(c -> event.tile.dst(c.x, c.y) < PandorumPlugin.config.alertDistance)){
+            Groups.player.each(p -> !PandorumPlugin.alertIgnores.contains(p.uuid()), p -> bundled(p, "events.withdraw-thorium", colorizedName(event.player), event.tile.tileX(), event.tile.tileY()));
         }
     }
 }
