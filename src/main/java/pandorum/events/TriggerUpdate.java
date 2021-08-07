0b9172316faa99834db10e57fcc8eaf9b6ab40cb
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-07 14:28:00 +0300

    Fix

--- src/main/java/pandorum/events/TriggerUpdate.java
+++ src/main/java/pandorum/events/TriggerUpdate.java
@@ -1,7 +1,9 @@
 package pandorum.events;
 
+import arc.util.Time;
 import mindustry.gen.Player;
 import mindustry.gen.Groups;
+import mindustry.gen.Unit;
 
 import static pandorum.effects.Effects.onJoin;
 import pandorum.PandorumPlugin;
@@ -12,16 +14,16 @@ public class TriggerUpdate {
     public static void call() {
         Groups.player.each(p -> p.unit().moving(), p -> onMove(p));
         if(PandorumPlugin.config.type == PluginType.sand || PandorumPlugin.config.type == PluginType.anarchy) {          
-            final float despawnDelay = Core.settings.getFloat("despawndelay", this.defDelay);
+            final float despawnDelay = Core.settings.getFloat("despawndelay", PandorumPlugin.defDelay);
             Groups.unit.each(unit -> {
-                if (Time.globalTime - (float)this.timer.get(unit, () -> Time.globalTime) >= despawnDelay) {
+                if (Time.globalTime - (float)PandorumPlugin.timer.get(unit, () -> Time.globalTime) >= despawnDelay) {
                     unit.spawnedByCore(true);
                 }
             });
-            for (final Unit key : this.timer.keys()) {
+            for (final Unit key : PandorumPlugin.timer.keys()) {
                 if (key == null) return;
                 if (key.isValid()) continue;
-                this.timer.remove(key);
+                PandorumPlugin.timer.remove(key);
             }
         }
     }
