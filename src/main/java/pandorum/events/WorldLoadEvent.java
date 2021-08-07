140b395641acc21ae4103d394b050d792caeed89
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-07 18:02:43 +0300

    Update WorldLoadEvent.java

--- src/main/java/pandorum/events/WorldLoadEvent.java
+++ src/main/java/pandorum/events/WorldLoadEvent.java
@@ -2,18 +2,21 @@ package pandorum.events;
 
 import mindustry.game.EventType;
 import mindustry.Vars;
+import mindustry.world.Tile;
 
 import pandorum.PandorumPlugin;
 import pandorum.struct.*;
 import pandorum.comp.Config.PluginType;
+
+import java.time.Duration;
 
 public class WorldLoadEvent {
     public static void call(final EventType.WorldLoadEvent event) {
         if(PandorumPlugin.config.type == PluginType.sand) PandorumPlugin.timer.clear();
         PandorumPlugin.history = new CacheSeq[Vars.world.width()][Vars.world.height()];
 
-        for(Tile tile : world.tiles){
-            history[tile.x][tile.y] = Seqs.newBuilder()
+        for(Tile tile : Vars.world.tiles){
+            PandorumPlugin.history[tile.x][tile.y] = Seqs.newBuilder()
                     .maximumSize(PandorumPlugin.config.historyLimit)
                     .expireAfterWrite(Duration.ofMillis(PandorumPlugin.config.expireDelay))
                     .build();
