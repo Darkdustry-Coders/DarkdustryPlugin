88b7b8956a0f39461db6ae2b0c64d68051300ecb
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-07 18:20:39 +0300

    Last events fix

--- src/main/java/pandorum/events/ActionFilter.java
+++ src/main/java/pandorum/events/ActionFilter.java
@@ -13,7 +13,7 @@ public class ActionFilter {
     public static boolean call(final Administration.PlayerAction action) {
         if(action.type == Administration.ActionType.rotate){
             Building building = action.tile.build;
-            CacheSeq<HistoryEntry> entries = history[action.tile.x][action.tile.y];
+            CacheSeq<HistoryEntry> entries = PandorumPlugin.history[action.tile.x][action.tile.y];
             HistoryEntry entry = new RotateEntry(Misc.colorizedName(action.player), building.block, action.rotation);
             entries.add(entry);
         }
