0e394fe170acc392f65e5fbf30b37a12d05a58ec
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-07 10:33:09 +0300

    Uh

---
+++ src/main/java/pandorum/events/PlayerJoinEvent.java
@@ -0,0 +1,26 @@
+package pandorum.events;
+
+import static pandorum.PandorumPlugin.*;
+import static pandorum.Misc.*;
+import static pandorum.effects.Effects.onJoin;
+
+import pandorum.comp.*;
+
+import mindustry.game.*;
+import mindustry.gen.*;
+
+public class PlayerJoinEvent {
+    public void call(final EventType.PlayerJoin event) {
+        forbiddenIps.each(i -> i.matchIp(event.player.con.address), i -> event.player.con.kick(Bundle.get("events.vpn-ip", findLocale(event.player.locale))));
+        if(config.bannedNames.contains(event.player.name())) event.player.con.kick(Bundle.get("events.unofficial-mindustry", findLocale(event.player.locale)), 60000);
+
+        sendToChat("server.player-join", colorizedName(event.player));
+        Log.info(event.player.name + " зашёл на сервер, IP: " + event.player.ip() + ", ID: " + event.player.uuid());
+
+        onJoin(event.player);
+
+        if(config.type == PluginType.anarchy || event.player.uuid().equals("GYmJmGDY2McAAAAAN8z4Bg==")) event.player.admin = true; //TODO добавить uuid главных админов
+        Call.infoMessage(event.player.con, Bundle.format("server.hellomsg", findLocale(event.player.locale)));
+        bundled(event.player, "server.motd");
+    }
+}
