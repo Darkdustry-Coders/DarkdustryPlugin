ad1c3e25aeacd4a49e32009e87ca4460f5c06800
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-07 13:51:14 +0300

    Update GameOverEvent.java

--- src/main/java/pandorum/events/GameOverEvent.java
+++ src/main/java/pandorum/events/GameOverEvent.java
@@ -2,12 +2,13 @@ package pandorum.events;
 
 import mindustry.game.EventType;
 import pandorum.PandorumPlugin;
+import pandorum.comp.Config.PluginType;
 
 public class GameOverEvent {
     public static void call(final EventType.GameOverEvent event) {
         if(PandorumPlugin.config.type == PluginType.other) return;
-        else if(PandorumPlugin.config.type == PluginType.pvp) surrendered.clear();
-        votesRTV.clear();
-        votesVNW.clear();
+        else if(PandorumPlugin.config.type == PluginType.pvp) PandorumPlugin.surrendered.clear();
+        PandorumPlugin.votesRTV.clear();
+        PandorumPlugin.votesVNW.clear();
     }
 }
