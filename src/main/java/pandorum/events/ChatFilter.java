20518d03b80d2d35ccf57019457d6c1852644825
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-08 09:54:51 +0300

    Create ChatFilter.java

---
+++ src/main/java/pandorum/events/ChatFilter.java
@@ -0,0 +1,12 @@
+package pandorum.events;
+
+import mindustry.gen.Player;
+
+import pandorum.Misc.*;
+import pandorum.PandorumPlugin;
+
+public class ChatFilter {
+    public static String call(final Player player, final String text) {
+        return null;
+    }
+}
