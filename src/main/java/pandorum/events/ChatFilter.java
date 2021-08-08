e683e365a861a5feedafbafa717a516625359cff
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-08 13:47:34 +0300

    Fix

--- src/main/java/pandorum/events/ChatFilter.java
+++ src/main/java/pandorum/events/ChatFilter.java
@@ -8,23 +8,27 @@ import pandorum.Misc.*;
 import pandorum.PandorumPlugin;
 import pandorum.comp.*;
 
+import java.io.IOException;
+
 public class ChatFilter {
     public static String call(final Player player, final String text) {
         Call.sendMessage(player.name + " [lightgray]>>[white] " + text);
         Log.info(player.name + "&ly > " + text);
 
         if (PandorumPlugin.config.hasWebhookLink()) {
-            Webhook wh = new Webhook(PandorumPlugin.config.DiscordWebhookLink);
-            wh.setUsername(player.name);
-            wh.setContent(text);
-            try {
-                wh.execute();
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            } finally {
-                wh=null;
-            }
-        }
+            new Thread(() -> {
+                Webhook wh = new Webhook(PandorumPlugin.config.DiscordWebhookLink);
+                wh.setUsername(player.name);
+                wh.setContent(text);
+                try {
+                    wh.execute();
+                } catch (IOException ioException) {
+                     ioException.printStackTrace();
+                } finally {
+                    Thread.currentThread().interrupt();
+                }
+                return;
+             }).start();
         return null;
     }
 }
