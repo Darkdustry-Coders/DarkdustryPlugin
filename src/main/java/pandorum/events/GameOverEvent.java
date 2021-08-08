00193bd356ba2ae4aaa771c8bb5b5d16492c1e70
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-08 13:33:53 +0300

    Update GameOverEvent.java

--- src/main/java/pandorum/events/GameOverEvent.java
+++ src/main/java/pandorum/events/GameOverEvent.java
@@ -3,6 +3,10 @@ package pandorum.events;
 import mindustry.game.EventType;
 import pandorum.PandorumPlugin;
 import pandorum.comp.Config.PluginType;
+import pandorum.comp.*;
+
+import java.io.IOException;
+import java.awt.Color;
 
 public class GameOverEvent {
     public static void call(final EventType.GameOverEvent event) {
@@ -10,5 +14,20 @@ public class GameOverEvent {
         else if(PandorumPlugin.config.type == PluginType.pvp) PandorumPlugin.surrendered.clear();
         PandorumPlugin.votesRTV.clear();
         PandorumPlugin.votesVNW.clear();
+
+        if (!PandorumPlugin.config.hasWebhookLink()) return;
+
+        Webhook wh = new Webhook(PandorumPlugin.config.DiscordWebhookLink);
+        wh.setUsername("Сервер");
+        wh.addEmbed(new Webhook.EmbedObject()
+                .setTitle("Игра окончена!")         
+                .setColor(new Color(200, 150, 255)));
+        try {
+            wh.execute();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } finally {
+            wh=null;
+        }
     }
 }
