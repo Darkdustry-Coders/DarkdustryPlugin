d6065e3321e8245c9ade8b75dea80c0fdc4112c1
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-08 09:59:58 +0300

    Test test test

--- src/main/java/pandorum/events/ChatFilter.java
+++ src/main/java/pandorum/events/ChatFilter.java
@@ -7,6 +7,8 @@ import pandorum.PandorumPlugin;
 
 public class ChatFilter {
     public static String call(final Player player, final String text) {
+        Call.sendMessage(player.name + " [lightgray]>>[white] " + text);
+        Log.info(player.name + "&ly > " + text);
         return null;
     }
 }
