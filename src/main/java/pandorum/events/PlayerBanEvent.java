517438439cb5fd0601b4179f2b5195fb65c2bc14
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-08 18:04:08 +0300

    Create PlayerBanEvent.java

---
+++ src/main/java/pandorum/events/PlayerBanEvent.java
@@ -0,0 +1,31 @@
+package pandorum.events;
+
+import mindustry.game.EventType;
+import pandorum.comp.*;
+import pandorum.PandorumPlugin;
+
+import java.io.IOException;
+import java.awt.Color;
+
+public class PlayerBanEvent {
+    public static void call(final EventType.PlayerBanEvent event) {
+        if (PandorumPlugin.config.hasWebhookLink()) {
+            new Thread(() -> {
+                Webhook wh = new Webhook(PandorumPlugin.config.DiscordWebhookLink);
+                wh.setUsername("Сервер");
+                wh.addEmbed(new Webhook.EmbedObject()
+                        .setTitle("Игрок получил бан!")
+                        .addField("Никнейм:", event.player.name, false)
+                        .setColor(new Color(255, 0, 0)));
+                try {
+                    wh.execute();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                } finally {
+                    Thread.currentThread().interrupt();
+                }
+                return;
+            }).start();
+        }
+    }
+}
