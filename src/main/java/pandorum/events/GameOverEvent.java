6638ed76dcc3036eff487b5d089ea2c96d9c302b
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-08 14:16:37 +0300

    Update GameOverEvent.java

--- src/main/java/pandorum/events/GameOverEvent.java
+++ src/main/java/pandorum/events/GameOverEvent.java
@@ -10,24 +10,27 @@ import java.awt.Color;
 
 public class GameOverEvent {
     public static void call(final EventType.GameOverEvent event) {
+        if (PandorumPlugin.config.hasWebhookLink()) {
+            new Thread(() -> {
+                Webhook wh = new Webhook(PandorumPlugin.config.DiscordWebhookLink);
+                wh.setUsername("Сервер");
+                wh.addEmbed(new Webhook.EmbedObject()
+                        .setTitle("Игра окончена!")         
+                        .setColor(new Color(0, 222, 222)));
+                try {
+                    wh.execute();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                } finally {
+                    Thread.currentThread().interrupt();
+                }
+                return;
+            }).start();
+        }
+
         if(PandorumPlugin.config.type == PluginType.other) return;
         else if(PandorumPlugin.config.type == PluginType.pvp) PandorumPlugin.surrendered.clear();
         PandorumPlugin.votesRTV.clear();
         PandorumPlugin.votesVNW.clear();
-
-        if (!PandorumPlugin.config.hasWebhookLink()) return;
-
-        Webhook wh = new Webhook(PandorumPlugin.config.DiscordWebhookLink);
-        wh.setUsername("Сервер");
-        wh.addEmbed(new Webhook.EmbedObject()
-                .setTitle("Игра окончена!")         
-                .setColor(new Color(0, 222, 222)));
-        try {
-            wh.execute();
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        } finally {
-            wh=null;
-        }
     }
 }
