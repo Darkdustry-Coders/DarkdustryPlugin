755e9cbe73fc4ced7dd320f5c48e2c321e4c4be5
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-08 13:39:54 +0300

    Update WorldLoadEvent.java

--- src/main/java/pandorum/events/WorldLoadEvent.java
+++ src/main/java/pandorum/events/WorldLoadEvent.java
@@ -7,8 +7,11 @@ import mindustry.world.Tile;
 import pandorum.PandorumPlugin;
 import pandorum.struct.*;
 import pandorum.comp.Config.PluginType;
+import pandorum.comp.*;
 
 import java.time.Duration;
+import java.io.IOException;
+import java.awt.Color;
 
 public class WorldLoadEvent {
     public static void call(final EventType.WorldLoadEvent event) {
@@ -20,6 +23,22 @@ public class WorldLoadEvent {
                     .maximumSize(PandorumPlugin.config.historyLimit)
                     .expireAfterWrite(Duration.ofMillis(PandorumPlugin.config.expireDelay))
                     .build();
+        }
+
+        if (!PandorumPlugin.config.hasWebhookLink()) return;
+
+        Webhook wh = new Webhook(PandorumPlugin.config.DiscordWebhookLink);
+        wh.setUsername("Сервер");
+        wh.addEmbed(new Webhook.EmbedObject()
+                .setTitle("Загружена новая карта!")
+                .addField("Название:", Vars.state.map.name(), false)
+                .setColor(new Color(0, 222, 222)));
+        try {
+            wh.execute();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } finally {
+            wh=null;
         }
     }
 }
