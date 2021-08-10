98a1f8255450cb0b4a2e290e8b4f037f9b58d284
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-10 17:57:22 +0300

    Ну чтож

---
+++ src/main/java/pandorum/comp/DiscordSender.java
@@ -0,0 +1,28 @@
+package pandorum.comp;
+
+import java.awt.*;
+import java.io.*;
+
+import pandorum.PandorumPlugin;
+
+public class DiscordSender {
+    public static void send(String name, String title, Color color) {
+        if (PandorumPlugin.config.hasWebhookLink()) {
+            new Thread(() -> {
+                Webhook wh = new Webhook(PandorumPlugin.config.DiscordWebhookLink);
+                wh.setUsername(name);
+                wh.addEmbed(new Webhook.EmbedObject()
+                         .setTitle(title)
+                         .setColor(color));
+                try {
+                    wh.execute();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                } finally {
+                    Thread.currentThread().interrupt();
+                }
+                return;
+            }).start();
+        }
+    }
+}
