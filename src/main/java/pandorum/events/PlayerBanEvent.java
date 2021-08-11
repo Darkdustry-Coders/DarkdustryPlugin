0ffea35d5fb528b7e2e7e8b7d1c9fbe07f03bf9b
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-11 11:10:21 +0300

    Update PlayerBanEvent.java

--- src/main/java/pandorum/events/PlayerBanEvent.java
+++ src/main/java/pandorum/events/PlayerBanEvent.java
@@ -6,31 +6,12 @@ import mindustry.Vars;
 import pandorum.comp.*;
 import pandorum.PandorumPlugin;
 
-import java.io.IOException;
 import java.awt.Color;
 
 public class PlayerBanEvent {
     public static void call(final EventType.PlayerBanEvent event) {
-        if (PandorumPlugin.config.hasWebhookLink()) {
-            PlayerInfo info = Vars.netServer.admins.getInfo(event.uuid);
-            if (info == null) return;
-            new Thread(() -> {
-                Webhook wh = new Webhook(PandorumPlugin.config.DiscordWebhookLink);
-                wh.setUsername("Сервер");
-                wh.addEmbed(new Webhook.EmbedObject()
-                        .setTitle("Игрок получил бан!")
-                        .addField("Никнейм:", info.lastName, false)
-                        .addField("IP:", info.lastIP, false)
-                        .setColor(new Color(255, 0, 0)));
-                try {
-                    wh.execute();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                } finally {
-                    Thread.currentThread().interrupt();
-                }
-                return;
-            }).start();
-        }
+        PlayerInfo info = Vars.netServer.admins.getInfo(event.uuid);
+        if (info == null) return;
+        DiscordSender.send("Сервер", "Игрок получил бан!", "Никнейм:", info.lastName, "IP:", lastIP, new Color(255, 0, 0));
     }
 }
