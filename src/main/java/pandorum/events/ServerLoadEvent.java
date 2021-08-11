91e51b5d2d78cfa5d254d1560e61af0ca1ac790c
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-11 09:34:04 +0300

    И ещё 🧹

--- src/main/java/pandorum/events/ServerLoadEvent.java
+++ src/main/java/pandorum/events/ServerLoadEvent.java
@@ -2,29 +2,11 @@ package pandorum.events;
 
 import mindustry.game.EventType;
 import pandorum.comp.*;
-import pandorum.PandorumPlugin;
 
-import java.io.IOException;
 import java.awt.Color;
 
 public class ServerLoadEvent {
     public static void call(final EventType.ServerLoadEvent event) {
-        if (PandorumPlugin.config.hasWebhookLink()) {
-            new Thread(() -> {
-                Webhook wh = new Webhook(PandorumPlugin.config.DiscordWebhookLink);
-                wh.setUsername("Сервер");
-                wh.addEmbed(new Webhook.EmbedObject()
-                        .setTitle("Сервер запущен!")         
-                        .setColor(new Color(0, 255, 0)));
-                try {
-                    wh.execute();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                } finally {
-                    Thread.currentThread().interrupt();
-                }
-                return;
-            }).start();
-        }
+        DiscordSender.send("Сервер", "Сервер запущен!", new Color(191, 255, 0));
     }
 }
