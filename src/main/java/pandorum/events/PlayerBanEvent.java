1efcb1421281414f4a3372a1eca29536d95b14c3
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-11 11:12:39 +0300

    Update PlayerBanEvent.java

--- src/main/java/pandorum/events/PlayerBanEvent.java
+++ src/main/java/pandorum/events/PlayerBanEvent.java
@@ -12,6 +12,6 @@ public class PlayerBanEvent {
     public static void call(final EventType.PlayerBanEvent event) {
         PlayerInfo info = Vars.netServer.admins.getInfo(event.uuid);
         if (info == null) return;
-        DiscordSender.send("Сервер", "Игрок получил бан!", "Никнейм:", info.lastName, "IP:", lastIP, new Color(255, 0, 0));
+        DiscordSender.send("Сервер", "Игрок получил бан!", "Никнейм:", info.lastName, "IP:", info.lastIP, new Color(255, 0, 0));
     }
 }
