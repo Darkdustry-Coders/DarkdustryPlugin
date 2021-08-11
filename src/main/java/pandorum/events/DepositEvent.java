bc508d9eca0427b9d00da6cfd2b19482e2c4c8da
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-11 20:23:26 +0300

    Update DepositEvent.java

--- src/main/java/pandorum/events/DepositEvent.java
+++ src/main/java/pandorum/events/DepositEvent.java
@@ -14,6 +14,7 @@ public class DepositEvent {
         if(PandorumPlugin.config.type == PluginType.other) return;
         if(event.tile.block() == Blocks.thoriumReactor && event.item == Items.thorium && event.player.team().cores().contains(c -> event.tile.dst(c.x, c.y) < PandorumPlugin.config.alertDistance)){
             Groups.player.each(p -> !PandorumPlugin.alertIgnores.contains(p.uuid()), p -> bundled(p, "events.withdraw-thorium", colorizedName(event.player), event.tile.tileX(), event.tile.tileY()));
+            DiscordSender.send(colorizedName(target), "ВНИМАНИЕ!!! Данный игрок положил торий в реактор возле ядра!", "X:", Short.toString(event.tile.tileX()), "Y:", Short.toString(event.tile.tileY()), new Color(204, 82, 27));
         }
     }
 }
