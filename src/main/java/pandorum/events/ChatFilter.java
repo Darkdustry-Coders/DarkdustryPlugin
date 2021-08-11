7b1f2cb757467d200df037a1ac43b11b6065a429
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-11 12:58:35 +0300

    Оставил облака чата над игроками

--- src/main/java/pandorum/events/ChatFilter.java
+++ src/main/java/pandorum/events/ChatFilter.java
@@ -10,9 +10,7 @@ import pandorum.Misc;
 
 public class ChatFilter {
     public static String call(final Player player, final String text) {
-        Call.sendMessage(Misc.colorizedName(player) + " [lightgray]>>[white] " + text);
-        Log.info(player.name + "&ly > " + text);
         DiscordSender.send(player.name, text);
-        return null;
+        return text;
     }
 }
