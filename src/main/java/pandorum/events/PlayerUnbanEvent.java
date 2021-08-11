d0b1187357aaaaf10211efc2f04ed8b723cff9ec
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-11 11:13:22 +0300

    Update PlayerUnbanEvent.java

--- src/main/java/pandorum/events/PlayerUnbanEvent.java
+++ src/main/java/pandorum/events/PlayerUnbanEvent.java
@@ -6,30 +6,12 @@ import mindustry.Vars;
 import pandorum.comp.*;
 import pandorum.PandorumPlugin;
 
-import java.io.IOException;
 import java.awt.Color;
 
 public class PlayerUnbanEvent {
     public static void call(final EventType.PlayerUnbanEvent event) {
-        if (PandorumPlugin.config.hasWebhookLink()) {
-            PlayerInfo info = Vars.netServer.admins.getInfo(event.uuid);
-            if (info == null) return;
-            new Thread(() -> {
-                Webhook wh = new Webhook(PandorumPlugin.config.DiscordWebhookLink);
-                wh.setUsername("Сервер");
-                wh.addEmbed(new Webhook.EmbedObject()
-                        .setTitle("Игрок получил разбан!")
-                        .addField("Никнейм:", info.lastName, false)
-                        .setColor(new Color(0, 0, 255)));
-                try {
-                    wh.execute();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                } finally {
-                    Thread.currentThread().interrupt();
-                }
-                return;
-            }).start();
-        }
+        PlayerInfo info = Vars.netServer.admins.getInfo(event.uuid);
+        if (info == null) return;
+        DiscordSender.send("Сервер", "Игрок был разбанен!", "Никнейм:", info.lastName, "IP:", info.lastIP, new Color(255, 0, 0));
     }
 }
