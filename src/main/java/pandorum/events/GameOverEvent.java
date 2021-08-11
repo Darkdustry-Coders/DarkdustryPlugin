5b78d0e67b7f8055ba9387b05ada3c1d7e2fcc1d
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-11 09:37:16 +0300

    И ещё чутка

--- src/main/java/pandorum/events/GameOverEvent.java
+++ src/main/java/pandorum/events/GameOverEvent.java
@@ -10,23 +10,7 @@ import java.awt.Color;
 
 public class GameOverEvent {
     public static void call(final EventType.GameOverEvent event) {
-        if (PandorumPlugin.config.hasWebhookLink()) {
-            new Thread(() -> {
-                Webhook wh = new Webhook(PandorumPlugin.config.DiscordWebhookLink);
-                wh.setUsername("Сервер");
-                wh.addEmbed(new Webhook.EmbedObject()
-                        .setTitle("Игра окончена!")         
-                        .setColor(new Color(0, 222, 222)));
-                try {
-                    wh.execute();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                } finally {
-                    Thread.currentThread().interrupt();
-                }
-                return;
-            }).start();
-        }
+        DiscordSender.send("Сервер", "Игра окончена!", new Color(0, 222, 222));
 
         if(PandorumPlugin.config.type == PluginType.other) return;
         else if(PandorumPlugin.config.type == PluginType.pvp) PandorumPlugin.surrendered.clear();
