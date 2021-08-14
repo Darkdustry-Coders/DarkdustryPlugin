42c1bbd8ad484506be7989bed95652be8f4693d0
Kvantuum-source <57044076+Kvantuum-source@users.noreply.github.com>
2021-08-14 21:17:44 +0300

    Анти чек нас всех спасёт

--- src/main/java/pandorum/events/WorldLoadEvent.java
+++ src/main/java/pandorum/events/WorldLoadEvent.java
@@ -15,9 +15,10 @@ import pandorum.struct.Seqs;
 import java.time.Duration;
 
 public class WorldLoadEvent {
+    @SuppressWarnings("unchecked")
     public static void call(final EventType.WorldLoadEvent event) {
         if(PandorumPlugin.config.type == PluginType.sand) PandorumPlugin.timer.clear();
-        PandorumPlugin.history = new CacheSeq<HistoryEntry>[Vars.world.width()][Vars.world.height()];
+        PandorumPlugin.history = new CacheSeq[Vars.world.width()][Vars.world.height()];
 
         for(Tile tile : Vars.world.tiles){
             PandorumPlugin.history[tile.x][tile.y] = Seqs.newBuilder()
