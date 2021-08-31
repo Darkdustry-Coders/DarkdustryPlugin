7923f9bbaa1a8c3adcca36827124c6c778451696
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-31 11:08:12 +0300

    Изменил цвет эффекта

--- src/main/java/pandorum/effects/Effects.java
+++ src/main/java/pandorum/effects/Effects.java
@@ -4,9 +4,9 @@ import mindustry.gen.*;
 
 public class Effects {
 
-    private static final EffectObject moveEffect = new EffectObject(0, 0, 30, "#4169e1ff", "freezing");
-    private static final EffectObject leaveEffect = new EffectObject(0, 0, 30, "#4169e1ff", "greenLaserCharge");
-    private static final EffectObject joinEffect = new EffectObject(0, 0, 30, "#4169e1ff", "greenBomb");
+    private static final EffectObject moveEffect = new EffectObject(0, 0, 30, "#4169e1", "freezing");
+    private static final EffectObject leaveEffect = new EffectObject(0, 0, 30, "#4169e1", "greenLaserCharge");
+    private static final EffectObject joinEffect = new EffectObject(0, 0, 30, "#4169e1", "greenBomb");
 
     public static void onMove(Player p) {
         try { moveEffect.spawn(p.x, p.y); }
