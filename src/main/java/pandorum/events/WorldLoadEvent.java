a88e67437a5d6ed57d0a188a91e84ed0a38e151b
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-09-01 21:17:53 +0300

    Починил автопаузу

--- src/main/java/pandorum/events/WorldLoadEvent.java
+++ src/main/java/pandorum/events/WorldLoadEvent.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import club.minnced.discord.webhook.send.WebhookEmbed;
 import club.minnced.discord.webhook.send.WebhookEmbedBuilder;
 import mindustry.Vars;
+import mindustry.gen.Groups;
 import mindustry.game.EventType;
 import mindustry.world.Tile;
 import pandorum.PandorumPlugin;
@@ -17,6 +18,7 @@ public class WorldLoadEvent {
     @SuppressWarnings("unchecked")
     public static void call(final EventType.WorldLoadEvent event) {
         if(PandorumPlugin.config.type == PluginType.sand) PandorumPlugin.timer.clear();
+        if (Groups.player.size() == 0) Vars.state.serverPaused = true;
         PandorumPlugin.history = new CacheSeq[Vars.world.width()][Vars.world.height()];
 
         for(Tile tile : Vars.world.tiles){
