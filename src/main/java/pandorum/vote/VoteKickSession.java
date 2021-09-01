4d29d0981907b6eb0e4e76a30b02e85484b11f31
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-09-01 10:03:07 +0300

    Сделал кикДурейшн публичным

--- src/main/java/pandorum/vote/VoteKickSession.java
+++ src/main/java/pandorum/vote/VoteKickSession.java
@@ -18,7 +18,7 @@ public class VoteKickSession{
     protected Task task;
     protected int votes;
 
-    protected int kickDuration = 30 * 60;
+    public int kickDuration = 30 * 60;
 
     public VoteKickSession(VoteKickSession[] map, Player target) {         
         this.target = target;
