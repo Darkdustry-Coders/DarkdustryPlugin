86c02bc2e1c7d7b5cbf86f9956242ef3e3111b04
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-09-02 10:59:42 +0300

    Убрал try

--- src/main/java/pandorum/effects/Effects.java
+++ src/main/java/pandorum/effects/Effects.java
@@ -4,22 +4,19 @@ import mindustry.gen.*;
 
 public class Effects {
 
-    private static final EffectObject moveEffect = new EffectObject(0, 0, 30, "#4169e1", "freezing");
+    private static final EffectObject moveEffect = new EffectObject(0, 0, 30, "#4169e1", "burning");
     private static final EffectObject leaveEffect = new EffectObject(0, 0, 30, "#4169e1", "greenLaserCharge");
     private static final EffectObject joinEffect = new EffectObject(0, 0, 30, "#4169e1", "greenBomb");
 
     public static void onMove(Player p) {
-        try { moveEffect.spawn(p.x, p.y); }
-        catch (NullPointerException e) {}
+        if(!player.dead()) moveEffect.spawn(p.x, p.y);
     }
 
     public static void onJoin(Player p) {        
-        try { joinEffect.spawn(p.x, p.y); }
-        catch (NullPointerException e) {}
+        if(!player.dead()) joinEffect.spawn(p.x, p.y);
     }
 
     public static void onLeave(Player p) {
-        try { leaveEffect.spawn(p.x, p.y); }
-        catch (NullPointerException e) {}
+        if(!player.dead()) leaveEffect.spawn(p.x, p.y);
     }
 }
