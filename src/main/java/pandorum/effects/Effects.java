8c8c3e5698caac0ec5a76ca94718e7ac5e57d321
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-09-02 12:02:47 +0300

    Чистка и фикс

--- src/main/java/pandorum/effects/Effects.java
+++ src/main/java/pandorum/effects/Effects.java
@@ -9,14 +9,14 @@ public class Effects {
     private static final EffectObject joinEffect = new EffectObject(0, 0, 30, "#4169e1", "greenBomb");
 
     public static void onMove(Player p) {
-        if(!player.dead()) moveEffect.spawn(p.x, p.y);
+        if(!p.dead()) moveEffect.spawn(p.x, p.y);
     }
 
     public static void onJoin(Player p) {        
-        if(!player.dead()) joinEffect.spawn(p.x, p.y);
+        if(!p.dead()) joinEffect.spawn(p.x, p.y);
     }
 
     public static void onLeave(Player p) {
-        if(!player.dead()) leaveEffect.spawn(p.x, p.y);
+        if(!p.dead()) leaveEffect.spawn(p.x, p.y);
     }
 }
