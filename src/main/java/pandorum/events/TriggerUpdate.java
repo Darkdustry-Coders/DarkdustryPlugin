a454e9189b188a6a20d64dc72090f9ee574b4116
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-09-04 14:04:07 +0300

    Чистка деспавна юнитов

--- src/main/java/pandorum/events/TriggerUpdate.java
+++ src/main/java/pandorum/events/TriggerUpdate.java
@@ -12,11 +12,9 @@ public class TriggerUpdate {
     public static void call() {
         Groups.player.each(p -> p.unit().moving(), Effects::onMove);
         if(PandorumPlugin.config.type == PluginType.sand || PandorumPlugin.config.type == PluginType.anarchy) {          
-            final float despawnDelay = Core.settings.getFloat("despawndelay", PandorumPlugin.defDelay);
+            final float despawnDelay = Core.settings.getFloat("despawndelay", 36000f);
             Groups.unit.each(unit -> {
-                if (Time.globalTime - PandorumPlugin.timer.get(unit, () -> Time.globalTime) >= despawnDelay) {
-                    unit.spawnedByCore(true);
-                }
+                if (Time.globalTime - PandorumPlugin.timer.get(unit, () -> Time.globalTime) >= despawnDelay) unit.spawnedByCore(true);
             });
             for (final Unit key : PandorumPlugin.timer.keys()) {
                 if (key == null) return;
