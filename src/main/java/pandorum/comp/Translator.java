295896564453d09682fc581a8d9904e03baf35c2
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-09-04 14:02:07 +0300

    Фикс переводчика

--- src/main/java/pandorum/comp/Translator.java
+++ src/main/java/pandorum/comp/Translator.java
@@ -9,13 +9,10 @@ import java.util.*;
 
 public class Translator {
 
-    // Переводит на нужный язык. Если не находит такой язык, то на русский
     public JSONObject translate(String text, String dest_lang) throws IOException, InterruptedException {
-        String destAlphaLang = codeLanguages.get("ru");
+        String destAlphaLang = PandorumPlugin.codeLanguages.get("ru");
 
-        if (this.codeLanguages.containsKey(dest_lang)) {
-            destAlphaLang = codeLanguages.get(dest_lang);
-        }
+        if (PandorumPlugin.codeLanguages.containsKey(dest_lang)) destAlphaLang = PandorumPlugin.codeLanguages.get(dest_lang);
 
         RequestBody formBody = new FormBody.Builder()
                 .add("to", destAlphaLang)
@@ -35,16 +32,13 @@ public class Translator {
                 .addHeader("sec-fetch-mode", "cors")
                 .addHeader("sec-fetch-site", "cross-site")
                 .build();
-        Response response = client.newCall(request).execute();
+        Response response = PandorumPlugin.client.newCall(request).execute();
         ResponseBody responseBody = response.body();
 
-        if (Objects.isNull(responseBody)) {
-            return new JSONObject("{}");
-        }
+        if (Objects.isNull(responseBody)) return new JSONObject("{}");
         return new JSONObject(responseBody.string());
     }
 
-    // Получает список всех возможных языков для перевода
     public JSONArray getAllLanguages() throws IOException {
         Request request = new Request.Builder()
                 .url("https://api-b2b.backenster.com/b1/api/v3/getLanguages?platform=dp")
@@ -60,7 +54,7 @@ public class Translator {
                 .addHeader("sec-fetch-site", "cross-site")
                 .addHeader("if-none-match", "W/\"aec6-7FjvQqCRl/1E+dvnCAlbAedDteg\"")
                 .build();
-        Response response = client.newCall(request).execute();
+        Response response = PandorumPlugin.client.newCall(request).execute();
         ResponseBody responseBody = response.body();
 
         if (Objects.isNull(responseBody)) {
