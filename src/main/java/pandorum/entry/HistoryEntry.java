7e252ff9cc75be24a6e4e1fd0f0e8508bb6c0027
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-09-04 21:30:47 +0300

    убрал выполненный тодо

--- src/main/java/pandorum/entry/HistoryEntry.java
+++ src/main/java/pandorum/entry/HistoryEntry.java
@@ -2,9 +2,8 @@ package pandorum.entry;
 
 import mindustry.gen.Player;
 
-public interface HistoryEntry{
+public interface HistoryEntry {
 
     String getMessage(Player player);
- 
-    //TODO сделать отображение времени, когда был изменён блок
+    
 }
