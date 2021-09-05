ccd0ac1b02d24998f30afb24cce906b2a48eb813
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-09-05 09:28:11 +0300

    Переводчик чата

--- src/main/java/pandorum/events/ChatFilter.java
+++ src/main/java/pandorum/events/ChatFilter.java
@@ -1,12 +1,55 @@
 package pandorum.events;
 
 import arc.util.Strings;
+import arc.util.Log;
 import mindustry.gen.Player;
+import mindustry.gen.Groups;
+
 import pandorum.comp.DiscordWebhookManager;
+import pandorum.comp.Translator;
+import pandorum.PandorumPlugin;
+
+import java.io.IOException;
+import java.util.*;
 
 public class ChatFilter {
-    public static String call(final Player player, final String text) {
-        DiscordWebhookManager.client.send(String.format("**[%s]:** %s", Strings.stripColors(player.name), text.replaceAll("https?://|@", "")));
-        return text;
+    public static String call(final Player author, final String text) {
+        author.sendMessage(text, author);
+        Log.info("&fi&lc@: &lw@", author.name, text);
+
+        Map<String, String> translationsCache = new HashMap<>();
+        Groups.player.each(player -> !player.equals(author), player -> {
+
+            String setting;
+            //setting = получаем инфу из БД.
+            if (setting.equals("off")) {
+                player.sendMessage(text, author);
+                return;
+            }
+            String language = setting.equals("auto") ? player.locale() : setting;
+
+            if (translationsCache.containsKey(language)) {
+                player.sendMessage(text + " [white]([gray]" + translationsCache.get(language) + "[white])", author);
+                return;
+            }
+
+            String translatedText = null;
+            try {
+                JSONObject translatorRes = translate(text, language);
+                if (!translatorRes.isNull("err") && translatorRes.optString("err").equals("")) {
+                    player.sendMessage(text, author);
+                    translationsCache.put(language, text);
+                    Log.info("Translator error: ", translatorRes.get("err"));
+                    return;
+                }
+                translatedText = translatorRes.getString("result");
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            translationsCache.put(language, translatedText);
+            player.sendMessage(text + " [white]([gray]" + translatedText + "[white])", author);
+        }
+        return null;
     }
 }
