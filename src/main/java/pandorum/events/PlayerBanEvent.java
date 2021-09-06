2a1dd5cca48214e1b066bf528366436a92a6c9c9
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-09-06 16:29:47 +0300

    Fix

--- src/main/java/pandorum/events/PlayerBanEvent.java
+++ src/main/java/pandorum/events/PlayerBanEvent.java
@@ -14,15 +14,15 @@ import org.bson.Document;
 public class PlayerBanEvent {
     public static void call(final EventType.PlayerBanEvent event) {
         Administration.PlayerInfo info = Vars.netServer.admins.getInfo(event.uuid);
-        if (info == null) return;
-        WebhookEmbedBuilder banEmbedBuilder = new WebhookEmbedBuilder()
-                .setColor(0xFF0000)
-                .setTitle(new WebhookEmbed.EmbedTitle("Игрок был заблокирован!", null))
-                .addField(new WebhookEmbed.EmbedField(true, "Никнейм", info.lastName))
-                .addField(new WebhookEmbed.EmbedField(true, "UUID", event.uuid))
-                .addField(new WebhookEmbed.EmbedField(true, "IP", info.lastIP));
-        DiscordWebhookManager.client.send(banEmbedBuilder.build());
-
+        if (info != null) {
+            WebhookEmbedBuilder banEmbedBuilder = new WebhookEmbedBuilder()
+                    .setColor(0xFF0000)
+                    .setTitle(new WebhookEmbed.EmbedTitle("Игрок был заблокирован!", null))
+                    .addField(new WebhookEmbed.EmbedField(true, "Никнейм", info.lastName))
+                    .addField(new WebhookEmbed.EmbedField(true, "UUID", event.uuid))
+                    .addField(new WebhookEmbed.EmbedField(true, "IP", info.lastIP));
+            DiscordWebhookManager.client.send(banEmbedBuilder.build());
+        }
         Document playerInfo = PandorumPlugin.playersInfo.find((playerInfo2) -> playerInfo2.getString("uuid").equals(event.uuid));
         if (playerInfo == null) {
             playerInfo = PandorumPlugin.playerInfoSchema.create(event.uuid, "IDK", true);
