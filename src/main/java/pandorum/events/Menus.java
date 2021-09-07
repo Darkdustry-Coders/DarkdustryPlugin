f04c4e8f1e2b8c60cd1ef40d231667659f3ad143
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-09-07 11:31:07 +0300

    Create Menus.java

---
+++ src/main/java/pandorum/events/Menus.java
@@ -0,0 +1,12 @@
+package pandorum.events;
+
+import mindustry.ui.Menus;
+
+public class Menus {
+    // Приветственное сообщение
+    Menus.registerMenu(1, (player, selection) -> {
+        if (selection == 0) {
+            // Отключаем приветственное сообщение
+        }
+    });
+}
