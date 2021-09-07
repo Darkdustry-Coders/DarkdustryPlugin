d6c213f075ee336646c0bba676fc0af5c47bdada
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-09-07 14:59:43 +0300

    Update MenuListener.java

--- src/main/java/pandorum/events/MenuListener.java
+++ src/main/java/pandorum/events/MenuListener.java
@@ -1,14 +1,16 @@
 package pandorum.events;
 
 import mindustry.ui.Menus;
+import org.bson.Document;
 
 public class MenuListener {
 
     static {
         // Приветственное сообщение
         Menus.registerMenu(1, (player, selection) -> {
-            if (selection == 0) {
-                // Отключаем приветственное сообщение
+            if (selection == 1) {
+                Document playerInfo = PandorumPlugin.playersInfo.find((playerInfo2) -> playerInfo2.getString("uuid").equals(event.player.uuid()));
+                playerInfo.replace("hellomsg", false);
             }
         });
     }
