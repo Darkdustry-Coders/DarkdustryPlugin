eba94ca7f34e44c62a9bc98b9104ccadf8e29cbf
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-09-07 13:30:29 +0300

    Чета типа так

--- src/main/java/pandorum/events/PlayerJoinEvent.java
+++ src/main/java/pandorum/events/PlayerJoinEvent.java
@@ -21,6 +21,8 @@ import pandorum.comp.Bundle;
 import pandorum.comp.Config.PluginType;
 import pandorum.comp.DiscordWebhookManager;
 import pandorum.effects.Effects;
+import pandorum.models.PlayerInfo;
+import org.bson.Document;
 
 public class PlayerJoinEvent {
     public static void call(final EventType.PlayerJoin event) {
@@ -37,15 +39,22 @@ public class PlayerJoinEvent {
 
         Effects.onJoin(event.player);
 
-        if (PandorumPlugin.config.type == PluginType.anarchy) event.player.admin(true);
+        Document playerInfo = PandorumPlugin.playersInfo.find((playerInfo2) -> playerInfo2.getString("uuid").equals(event.player.uuid()));
+        if (playerInfo == null) {
+            playerInfo = PandorumPlugin.playerInfoSchema.create(player.uuid(), true, false, "off");
+            PandorumPlugin.playersInfo.add(playerInfo);
+        }
 
-        String[][] options = {
-            {Bundle.format("events.hellomsg.ok", findLocale(event.player.locale))},
-            {Bundle.format("events.hellomsg.disable", findLocale(event.player.locale))}
-        };
-        Call.menu(event.player.con, 1, Bundle.format("events.hellomsg.header", findLocale(event.player.locale)), Bundle.format("events.hellomsg", findLocale(event.player.locale)), options);
+        if(playerInfo.getBoolean("hellomsg")) {
+            String[][] options = {
+                {Bundle.format("events.hellomsg.ok", findLocale(event.player.locale))},
+                {Bundle.format("events.hellomsg.disable", findLocale(event.player.locale))}
+            };
+            Call.menu(event.player.con, 1, Bundle.format("events.hellomsg.header", findLocale(event.player.locale)), Bundle.format("events.hellomsg", findLocale(event.player.locale)), options);
+        }
+        
         bundled(event.player, "events.motd");
-
+        if (PandorumPlugin.config.type == PluginType.anarchy) event.player.admin(true);
         WebhookEmbedBuilder banEmbedBuilder = new WebhookEmbedBuilder()
                 .setColor(0x00FF00)
                 .setTitle(new WebhookEmbed.EmbedTitle(String.format("%s зашёл на сервер!", Strings.stripColors(event.player.name())), null));
