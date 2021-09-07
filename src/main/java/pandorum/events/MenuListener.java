c2a625e90fb05d40211490dc1007e578743cb0ae
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-09-07 11:58:09 +0300

    Fix

--- src/main/java/pandorum/events/MenuListener.java
+++ src/main/java/pandorum/events/MenuListener.java
@@ -3,10 +3,13 @@ package pandorum.events;
 import mindustry.ui.Menus;
 
 public class MenuListener {
-    // Приветственное сообщение
-    Menus.registerMenu(1, (player, selection) -> {
-        if (selection == 0) {
-            // Отключаем приветственное сообщение
-        }
-    });
+
+    static {
+        // Приветственное сообщение
+        Menus.registerMenu(1, (player, selection) -> {
+            if (selection == 0) {
+                // Отключаем приветственное сообщение
+            }
+        });
+    }
 }
