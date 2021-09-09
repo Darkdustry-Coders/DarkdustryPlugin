712b5cbae7e7cfa3932821779444a50847fc6ecb
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-09-09 18:33:34 +0300

    Update VoteLoadSession.java

--- src/main/java/pandorum/vote/VoteLoadSession.java
+++ src/main/java/pandorum/vote/VoteLoadSession.java
@@ -80,13 +80,6 @@ public class VoteLoadSession extends VoteSession{
     }
 
     @Override
-    public void stop() {
-        voted.clear();
-        session[0] = null;
-        task.cancel();
-    }
-
-    @Override
     public int votesRequired() {
         return (int)Math.ceil(config.voteRatio * Groups.player.size());
     }
