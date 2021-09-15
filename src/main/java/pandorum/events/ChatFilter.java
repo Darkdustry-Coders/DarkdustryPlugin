4bcaa178a47adc7bd18282c0c215a2739d85cb02
Kvantuum-source <57044076+Kvantuum-source@users.noreply.github.com>
2021-09-15 18:50:44 +0300

    Асинхронность калаш мне в зад

--- src/main/java/pandorum/events/ChatFilter.java
+++ src/main/java/pandorum/events/ChatFilter.java
@@ -1,37 +1,33 @@
 package pandorum.events;
 
-import arc.util.Strings;
 import arc.util.Log;
-import mindustry.gen.Player;
+import arc.util.Strings;
 import mindustry.gen.Groups;
-
+import mindustry.gen.Player;
+import org.bson.Document;
+import pandorum.PandorumPlugin;
 import pandorum.comp.DiscordWebhookManager;
 import pandorum.comp.Translator;
-import pandorum.PandorumPlugin;
-import pandorum.models.PlayerInfo;
-
-import org.json.JSONObject;
-import org.bson.Document;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChatFilter {
     public static String call(final Player author, final String text) {
         author.sendMessage(text, author);
         Log.info("&fi&lc@: &lw@", author.name, text);
-
         Map<String, String> translationsCache = new HashMap<>();
         Groups.player.each(player -> !player.equals(author), player -> {
 
             Document playerInfo = PandorumPlugin.playersInfo.find((playerInfo2) -> playerInfo2.getString("uuid").equals(player.uuid()));
             if (playerInfo == null) {
-                playerInfo = PandorumPlugin.playerInfoSchema.create(player.uuid(), "IDK", false, "off");
+                playerInfo = PandorumPlugin.playerInfoSchema.create(player.uuid(), true, false, "off");
                 PandorumPlugin.playersInfo.add(playerInfo);
             }
             String locale = playerInfo.getString("locale");
 
-            if (locale == null) { 
+            if (locale == null || locale.equals("off")) { 
                 player.sendMessage(text, author);
                 return;
             }
@@ -43,23 +39,25 @@ public class ChatFilter {
                 return;
             }
 
-            String translatedText = null;
             try {
-                JSONObject translatorRes = Translator.translate(text, language);
-                if (!translatorRes.isNull("err") && translatorRes.optString("err").equals("")) {
-                    player.sendMessage(text, author);
-                    translationsCache.put(language, text);
-                    Log.info("Ошибка перевода: ", translatorRes.get("err"));
-                    return;
-                }
-                translatedText = translatorRes.getString("result");
+                Translator.translate(text, language, (translatorRes) -> {
+                    if (!translatorRes.isNull("err") && translatorRes.optString("err").equals("")) {
+                        player.sendMessage(text, author);
+                        translationsCache.put(language, text);
+                        Log.info("Ошибка перевода: ", translatorRes.get("err"));
+                        return;
+                    }
+
+                    String translatedText = translatorRes.getString("result");
+                    translationsCache.put(language, translatedText);
+                    player.sendMessage(text + " [white]([gray]" + translatedText + "[white])", author);
+                });
             } catch (IOException | InterruptedException e) {
                 e.printStackTrace();
             }
-
-            translationsCache.put(language, translatedText);
-            player.sendMessage(text + " [white]([gray]" + translatedText + "[white])", author);
         });
+
+        DiscordWebhookManager.client.send(String.format("**[%s]:** %s", Strings.stripColors(author.name), text.replaceAll("https?://|@", " ")));
         return null;
     }
 }
