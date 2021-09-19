0db2e5da07decd7f1acfe31a7d22c11da7a5a743
Darkness#3729 <79508138+Darkness6030@users.noreply.github.com>
2021-09-19 13:36:25 +0300

    Fix

--- src/main/java/pandorum/entry/WithdrawEntry.java
+++ src/main/java/pandorum/entry/WithdrawEntry.java
@@ -34,6 +34,6 @@ public class WithdrawEntry implements HistoryEntry{
         df.setTimeZone(TimeZone.getTimeZone(ZoneId.of("Europe/Moscow")));
         final String ftime = df.format(this.time);
 
-        return Bundle.format("history.withdraw", findLocale(player.locale), name, block.name, count, ConfigEntry.icons.get(item.name), ftime);
+        return Bundle.format("history.withdraw", findLocale(player.locale), name, count, ConfigEntry.icons.get(item.name), block.name, ftime);
     }
 }
