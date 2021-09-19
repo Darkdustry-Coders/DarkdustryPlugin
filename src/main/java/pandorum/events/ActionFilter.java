9ef12bc8804edad3ad2058880b2fee690fc841e2
Darkness#3729 <79508138+Darkness6030@users.noreply.github.com>
2021-09-19 11:58:58 +0300

    Чистка

--- src/main/java/pandorum/events/ActionFilter.java
+++ src/main/java/pandorum/events/ActionFilter.java
@@ -1,7 +1,7 @@
 package pandorum.events;
 
 import mindustry.gen.Building;
-import mindustry.net.Administration;
+import mindustry.net.Administration.ActionType;
 
 import pandorum.struct.*;
 import pandorum.entry.*;
@@ -10,20 +10,15 @@ import pandorum.Misc;
 
 public class ActionFilter {
     public static boolean call(final Administration.PlayerAction action) {
-        if (action.type == Administration.ActionType.rotate) {
-            Building building = action.tile.build;
+        HistoryEntry entry = switch(action.type) {
+            case ActionType.rotate -> new RotateEntry(Misc.colorizedName(action.player), action.tile.build.block, action.rotation);
+            case ActionType.withdrawItem -> new WithdrawEntry(Misc.colorizedName(action.player), action.tile.build.block, action.item, action.itemAmount);
+            case ActionType.depositItem -> new DepositEntry(Misc.colorizedName(action.player), action.tile.build.block, action.item, action.itemAmount);
+            default -> null;
+        };
+
+        if (entry != null) {
             CacheSeq<HistoryEntry> entries = PandorumPlugin.history[action.tile.x][action.tile.y];
-            HistoryEntry entry = new RotateEntry(Misc.colorizedName(action.player), building.block, action.rotation);
-            entries.add(entry);
-        } else if (action.type == Administration.ActionType.withdrawItem) {
-            Building building = action.tile.build;
-            CacheSeq<HistoryEntry> entries = PandorumPlugin.history[action.tile.x][action.tile.y];
-            HistoryEntry entry = new WithdrawEntry(Misc.colorizedName(action.player), building.block, action.item, action.itemAmount);
-            entries.add(entry);
-        } else if (action.type == Administration.ActionType.depositItem) {
-            Building building = action.tile.build;
-            CacheSeq<HistoryEntry> entries = PandorumPlugin.history[action.tile.x][action.tile.y];
-            HistoryEntry entry = new DepositEntry(Misc.colorizedName(action.player), building.block, action.item, action.itemAmount);
             entries.add(entry);
         }
         return true;
