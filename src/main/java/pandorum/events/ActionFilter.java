6c5d96f9d0303c3a865a61b4d18584372e92b0b3
Darkness#3729 <79508138+Darkness6030@users.noreply.github.com>
2021-09-19 10:52:12 +0300

    Тестовая фича

--- src/main/java/pandorum/events/ActionFilter.java
+++ src/main/java/pandorum/events/ActionFilter.java
@@ -10,11 +10,15 @@ import pandorum.Misc;
 
 public class ActionFilter {
     public static boolean call(final Administration.PlayerAction action) {
-        if(action.type == Administration.ActionType.rotate){
+        if (action.type == Administration.ActionType.rotate) {
             Building building = action.tile.build;
             CacheSeq<HistoryEntry> entries = PandorumPlugin.history[action.tile.x][action.tile.y];
             HistoryEntry entry = new RotateEntry(Misc.colorizedName(action.player), building.block, action.rotation);
             entries.add(entry);
+        } else if (action.type == Administration.ActionType.withdrawItem) {
+            // TODO
+        } else if (action.type == Administration.ActionType.depositItem) {
+            // TODO
         }
         return true;
     }
