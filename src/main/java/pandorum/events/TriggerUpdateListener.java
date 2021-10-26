fbf8dacd083730c21737425eaa4e38493595610e
Darkness#3729 <79508138+Darkness6030@users.noreply.github.com>
2021-10-26 10:42:03 +0300

    Фикс цвета никнейма

--- src/main/java/pandorum/events/TriggerUpdateListener.java
+++ src/main/java/pandorum/events/TriggerUpdateListener.java
@@ -9,7 +9,7 @@ public class TriggerUpdateListener {
     public static void call() {
         Groups.player.each(p -> p.unit().moving(), Effects::onMove);
         if (PandorumPlugin.interval.get(1, 30f)) {
-            Groups.player.each(player -> Ranks.getRank(player, rank -> player.name(rank.tag + player.getInfo().lastName)));
+            Groups.player.each(player -> Ranks.getRank(player, rank -> player.name(rank.tag + "[#" + player.color.toString().toUpperCase() + "]" + player.getInfo().lastName)));
         }
     }
 }
