c8bba0bced2be0349fbb7d3a242c2bcc7726cf06
Darkness#3729 <79508138+Darkness6030@users.noreply.github.com>
2021-10-30 11:35:33 +0300

    Update ARTVCommand.java

--- src/main/java/pandorum/commands/client/ARTVCommand.java
+++ src/main/java/pandorum/commands/client/ARTVCommand.java
@@ -4,6 +4,7 @@ import mindustry.gen.Call;
 import mindustry.gen.Player;
 import pandorum.Misc;
 import pandorum.comp.Bundle;
+import pandorum.events.MenuListener;
 
 import static pandorum.Misc.findLocale;
 
@@ -11,6 +12,6 @@ public class ARTVCommand implements ClientCommand {
     public static void run(final String[] args, final Player player) {
         if (Misc.adminCheck(player)) return;
         String[][] options = {{Bundle.format("events.menu.yes", findLocale(player.locale)), Bundle.format("events.menu.no", findLocale(player.locale))}};
-        Call.menu(player.con, 2, Bundle.format("commands.admin.artv.menu.header", findLocale(player.locale)), Bundle.format("commands.admin.artv.menu.content", findLocale(player.locale)), options);
+        Call.menu(player.con, MenuListener.artvMenu, Bundle.format("commands.admin.artv.menu.header", findLocale(player.locale)), Bundle.format("commands.admin.artv.menu.content", findLocale(player.locale)), options);
     }
 }
