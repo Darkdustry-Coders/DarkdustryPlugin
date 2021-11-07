b9d295d733013f67185655febee910dfa964e825
Darkness#3729 <79508138+Darkness6030@users.noreply.github.com>
2021-11-08 00:28:11 +0300

    Update anti-vpn

--- src/main/java/pandorum/comp/AntiVpn.java
+++ src/main/java/pandorum/comp/AntiVpn.java
@@ -1,30 +1,67 @@
-package pandorum.comp;
+package pandorum.comp
 
-import arc.util.Log;
-import io.ipinfo.api.errors.ErrorResponseException;
-import io.ipinfo.api.errors.RateLimitedException;
-import io.ipinfo.api.model.IPResponse;
-import pandorum.PandorumPlugin;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.function.Consumer;
 
-public class AntiVpn {
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.HttpUrl;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
-    public static boolean enabled = true;
+import org.json.JSONObject;
 
-    /**
-     * @param ip - Айпи, который нужно проверить
-     * @return - Является ли этот айпи VPN
-     */
-    public static boolean checkIP(String ip) {
-        if (enabled) {
-            try {
-                IPResponse response = PandorumPlugin.ipInfo.lookupIP(ip);
-                return response.getPrivacy().getVpn();
-            } catch (RateLimitedException e) {
-                Log.info("Лимит запросов ANTI-VPN исчерпан. Выключаю ANTI-VPN...");
-                enabled = false;
-            } catch (ErrorResponseException ignored) {}
-        }
+public class AntiVPN {
+    private String token;
+    private OkHttpClient client;
+    public static String API_VERSION = "v2";
 
-        return false;
+    public AntiVPN(String token) {
+        this.client = new OkHttpClient();
+        this.token = token;
+    }
+
+    public void isDangerousIp(String ip, Consumer<Boolean> callback) {
+        HttpUrl url = new HttpUrl.Builder()
+            .scheme("https")
+            .host("proxycheck.io")
+            .addPathSegment(API_VERSION)
+            .addPathSegment(ip)
+            .addQueryParameter("key", token)
+            .addQueryParameter("risk", "1")
+            .addQueryParameter("vpn", "1")
+            .build();
+        Request request = new Request.Builder()
+            .addHeader("accept", "application/json")
+            .url(url)
+            .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                callback.accept(false);
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                JSONObject body = new JSONObject(response.body().string());
+                JSONObject ipInfo = body.getJSONObject(ip);
+
+                int risk = ipInfo.getInt("risk");
+                String isProxy = ipInfo.getString("proxy");
+                String ipType = ipInfo.getString("type");
+
+                callback.accept(
+                        risk >= 66 || isProxy == "yes" || Set.of(
+                        "tor", "socks", "socks4", "socks4a",
+                        "socks5", "socks5h", "shadowsocks",
+                        "compromised server", "inference engine",
+                        "openvpn", "vpn"
+                    ).contains(ipType.toLowerCase())
+                );
+            }
+        });
     }
 }
