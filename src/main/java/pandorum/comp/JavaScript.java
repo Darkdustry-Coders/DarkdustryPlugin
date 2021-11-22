d1e4d37bae39327dc3533af227e1c278da4ef6d6
darkness <inflexibled@gmail.com>
2021-11-22 20:27:24 +0300

    Add more shizophrenia

--- src/main/java/pandorum/comp/JavaScript.java
+++ src/main/java/pandorum/comp/JavaScript.java
@@ -5,7 +5,9 @@ import arc.struct.Seq;
 public class JavaScript {
     private static final Seq<String> blacklist = Seq.with(".net.", "java.net", "files", "reflect", "javax", "rhino", "file", "channels", "jdk", "exit", "reset",
             "runtime", "util.os", "rmi", "security", "org.", "sun.", "beans", "sql", "http", "exec", "compiler", "process", "system", "app", "exception", "error",
-            ".awt", "socket", "classloader", "oracle", "invoke", "java.util.function", "java.util.stream", "org.", "mod.classmap", "config", "dispose");
+            ".awt", "socket", "classloader", "oracle", "invoke", "java.util.function", "java.util.stream", "org.", "mod.classmap", "config", "dispose", "delete",
+            "ban", "kick", "uuid", "packet", "while", "connect", "file", "settings", "throw"
+    );
 
     public static boolean allowScript(String text) {
         return !blacklist.contains(t -> text.toLowerCase().contains(t));
