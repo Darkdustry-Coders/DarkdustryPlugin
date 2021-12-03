134813f2c8d57107484645799eacba186407d306
Darkness#3729 <79508138+Darkness6030@users.noreply.github.com>
2021-12-03 09:23:59 +0300

    РЕЗНЯ

--- src/main/java/pandorum/comp/Authme.java
+++ src/main/java/pandorum/comp/Authme.java
@@ -5,29 +5,26 @@ import discord4j.core.object.component.Button;
 import discord4j.core.object.entity.Message;
 import mindustry.gen.Player;
 import pandorum.Misc;
-import pandorum.struct.Tuple2;
 
 import static mindustry.Vars.netServer;
 import static pandorum.Misc.bundled;
 
 public class Authme {
 
-    public static final ObjectMap<Message, Tuple2<String, String>> loginWaiting = new ObjectMap<>();
+    public static final ObjectMap<Message, Player> loginWaiting = new ObjectMap<>();
 
     public static final Button confirm = Button.success("confirm", "Подтвердить");
     public static final Button deny = Button.danger("deny", "Отклонить");
 
-    public static void confirm(String uuid) {
-        Player player = Misc.findByID(uuid);
+    public static void confirm(Player player) {
         if (player != null) {
-            player.admin(true);
             netServer.admins.adminPlayer(player.uuid(), player.usid());
+            player.admin(true);
             bundled(player, "commands.login.success");
         }
     }
 
-    public static void deny(String uuid) {
-        Player player = Misc.findByID(uuid);
+    public static void deny(Player player) {
         if (player != null) bundled(player, "commands.login.ignore");
     }
 }
