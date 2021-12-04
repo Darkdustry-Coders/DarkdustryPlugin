bbe3b518d4c86f2089074b7894aad76bf41f562e
Darkness#3729 <79508138+Darkness6030@users.noreply.github.com>
2021-12-04 11:12:46 +0300

    Update and rename DespawnCommand.java to ConsoleDespawnCommand.java

--- src/main/java/pandorum/commands/server/ConsoleDespawnCommand.java
+++ src/main/java/pandorum/commands/server/ConsoleDespawnCommand.java
@@ -4,7 +4,7 @@ import arc.util.Log;
 import mindustry.gen.Groups;
 import mindustry.gen.Unitc;
 
-public class DespawnCommand {
+public class ConsoleDespawnCommand {
     public static void run(final String[] args) {
         int amount = Groups.unit.size();
         Groups.unit.each(Unitc::kill);
