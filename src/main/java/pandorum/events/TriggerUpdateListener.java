b08603a8ddfe7022079432b0d67c47ac16ea1804
darkness <inflexibled@gmail.com>
2021-12-14 15:00:05 +0300

    fix

--- src/main/java/pandorum/events/TriggerUpdateListener.java
+++ src/main/java/pandorum/events/TriggerUpdateListener.java
@@ -11,11 +11,15 @@ public class TriggerUpdateListener {
 
     public static void update() {
         Groups.player.each(p -> p.unit().moving(), Effects::onMove);
+
         if (PandorumPlugin.interval.get(1, 60f)) {
-            Groups.player.each(player -> PlayerModel.find(new BasicDBObject("UUID", player.uuid()), playerInfo -> {
-                playerInfo.playTime += 1000L;
-                playerInfo.save();
-            }));
+            Groups.player.each(p -> {
+                PlayerModel.find(new BasicDBObject("UUID", p.uuid()), playerInfo -> {
+                    playerInfo.playTime += 1000L;
+                    playerInfo.save();
+                });
+                Ranks.updateName(p);
+            });
         }
     }
 }
