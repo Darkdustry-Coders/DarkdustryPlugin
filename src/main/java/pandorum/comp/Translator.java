ee557a036b996753a6964373457e26edda3a9d11
Darkness#3729 <79508138+Darkness6030@users.noreply.github.com>
2022-01-07 18:19:51 +0300

    Reznya

--- src/main/java/pandorum/comp/Translator.java
+++ src/main/java/pandorum/comp/Translator.java
@@ -15,7 +15,7 @@ import static pandorum.PluginVars.codeLanguages;
 public class Translator {
 
     private static final OkHttpClient client = new OkHttpClient();
-    private static final Request.Builder requestBuilder = new Request.Builder()
+    private static final Request.Builder translateRequest = new Request.Builder()
             .url("https://api-b2b.backenster.com/b1/api/v3/translate/")
             .addHeader("accept", "application/json, text/javascript, */*; q=0.01")
             .addHeader("accept-language", "ru,en;q=0.9")
@@ -26,6 +26,20 @@ public class Translator {
             .addHeader("sec-fetch-dest", "empty")
             .addHeader("sec-fetch-mode", "cors")
             .addHeader("sec-fetch-site", "cross-site");
+
+    private static final Request.Builder languagesRequest = new Request.Builder()
+            .url("https://api-b2b.backenster.com/b1/api/v3/getLanguages?platform=dp")
+            .get()
+            .addHeader("accept", "application/json, text/javascript, */*; q=0.01")
+            .addHeader("accept-language", "ru,en;q=0.9")
+            .addHeader("authorization", "Bearer a_25rccaCYcBC9ARqMODx2BV2M0wNZgDCEl3jryYSgYZtF1a702PVi4sxqi2AmZWyCcw4x209VXnCYwesx")
+            .addHeader("content-type", "application/x-www-form-urlencoded; charset=UTF-8")
+            .addHeader("sec-ch-ua", "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Yandex\";v=\"90\"")
+            .addHeader("sec-ch-ua-mobile", "?0")
+            .addHeader("sec-fetch-dest", "empty")
+            .addHeader("sec-fetch-mode", "cors")
+            .addHeader("sec-fetch-site", "cross-site")
+            .addHeader("if-none-match", "W/\"aec6-7FjvQqCRl/1E+dvnCAlbAedDteg\"");
 
     public Translator() {
         for (JsonElement languageElement : getAllLanguages()) {
@@ -38,7 +52,7 @@ public class Translator {
         String language = codeLanguages.get(lang, codeLanguages.get("en"));
 
         RequestBody formBody = new FormBody.Builder().add("to", language).add("text", text).add("platform", "dp").build();
-        Request request = requestBuilder.post(formBody).build();
+        Request request = translateRequest.post(formBody).build();
 
         client.newCall(request).enqueue(new Callback() {
             @Override
@@ -57,20 +71,7 @@ public class Translator {
     }
 
     public JsonArray getAllLanguages() {
-        Request request = new Request.Builder()
-                .url("https://api-b2b.backenster.com/b1/api/v3/getLanguages?platform=dp")
-                .get()
-                .addHeader("accept", "application/json, text/javascript, */*; q=0.01")
-                .addHeader("accept-language", "ru,en;q=0.9")
-                .addHeader("authorization", "Bearer a_25rccaCYcBC9ARqMODx2BV2M0wNZgDCEl3jryYSgYZtF1a702PVi4sxqi2AmZWyCcw4x209VXnCYwesx")
-                .addHeader("content-type", "application/x-www-form-urlencoded; charset=UTF-8")
-                .addHeader("sec-ch-ua", "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Yandex\";v=\"90\"")
-                .addHeader("sec-ch-ua-mobile", "?0")
-                .addHeader("sec-fetch-dest", "empty")
-                .addHeader("sec-fetch-mode", "cors")
-                .addHeader("sec-fetch-site", "cross-site")
-                .addHeader("if-none-match", "W/\"aec6-7FjvQqCRl/1E+dvnCAlbAedDteg\"")
-                .build();
+        Request request = languagesRequest.build();
 
         try {
             Response response = client.newCall(request).execute();
