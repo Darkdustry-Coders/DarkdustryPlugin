017bf929b1ade7e94d11879a984ce9727b0191e1
Darkness <inflexibled@gmail.com>
2022-01-15 11:53:33 +0300

    m

--- src/main/java/pandorum/events/listeners/DepositListener.java
+++ src/main/java/pandorum/events/listeners/DepositListener.java
@@ -15,9 +15,7 @@ public class DepositListener {
 
     public static void call(final DepositEvent event) {
         if (config.alertsEnabled() && event.tile.block instanceof NuclearReactor && event.item.explosiveness > 0f && event.player.team().cores().contains(c -> event.tile.dst(c.x, c.y) < alertsDistance)) {
-            Groups.player.each(p -> p.team() == event.player.team(), p -> PlayerModel.find(p.uuid(), playerInfo -> {
-                if (playerInfo.alerts) bundled(p, "events.withdraw-thorium", event.player.coloredName(), Icons.get(event.item.name), Icons.get(event.tile.block.name), event.tile.tileX(), event.tile.tileY());
-            }));
+            Groups.player.each(p -> p.team() == event.player.team(), p -> PlayerModel.find(p.uuid(), playerInfo -> playerInfo.alerts, playerInfo -> bundled(p, "events.withdraw-thorium", event.player.coloredName(), Icons.get(event.item.name), Icons.get(event.tile.block.name), event.tile.tileX(), event.tile.tileY())));
         }
 
         if (config.historyEnabled() && event.player != null) {
--- src/main/java/pandorum/models/PlayerModel.java
+++ src/main/java/pandorum/models/PlayerModel.java
@@ -1,5 +1,6 @@
 package pandorum.models;
 
+import arc.func.Boolf;
 import arc.func.Cons;
 import com.mongodb.BasicDBObject;
 import pandorum.database.MongoDataBridge;
@@ -17,6 +18,12 @@ public class PlayerModel extends MongoDataBridge<PlayerModel> {
 
     public static void find(String UUID, Cons<PlayerModel> cons) {
         findAndApplySchema(PlayerModel.class, new BasicDBObject("UUID", UUID), cons);
+    }
+
+    public static void find(String UUID, Boolf<PlayerModel> filter, Cons<PlayerModel> cons) {
+        findAndApplySchema(PlayerModel.class, new BasicDBObject("UUID", UUID), playerInfo -> {
+            if (filter.get(playerInfo)) cons.get(playerInfo);
+        });
     }
 }
 
