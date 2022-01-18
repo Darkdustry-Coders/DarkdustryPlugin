2a645e0b8f2ecdfcd66a27411d24b9e213699ad9
Даркнесс#3729 <79508138+Darkness6030@users.noreply.github.com>
2022-01-18 15:00:25 +0300

    Резня

--- src/main/java/pandorum/commands/server/DespawnCommand.java
+++ src/main/java/pandorum/commands/server/DespawnCommand.java
@@ -4,8 +4,15 @@ import arc.util.Log;
 import mindustry.gen.Groups;
 import mindustry.gen.Unitc;
 
+import static mindustry.Vars.state;
+
 public class DespawnCommand {
     public static void run(final String[] args) {
+        if (state.isMenu()) {
+            Log.err("Сервер отключен. Может быть, пора запустить его командой 'host'?");
+            return;
+        }
+
         int amount = Groups.unit.size();
         Groups.unit.each(Unitc::kill);
         Log.info("Убито @ юнитов...", amount);
