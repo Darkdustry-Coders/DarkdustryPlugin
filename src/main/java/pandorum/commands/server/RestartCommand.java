23944d408c1bb25fb14d264a34cee1f4c60d6351
Даркнесс#3729 <79508138+Darkness6030@users.noreply.github.com>
2022-01-18 14:22:52 +0300

    Update RestartCommand.java

--- src/main/java/pandorum/commands/server/RestartCommand.java
+++ src/main/java/pandorum/commands/server/RestartCommand.java
@@ -1,35 +1,20 @@
 package pandorum.commands.server;
 
 import arc.util.Log;
-import arc.util.Timer;
+import arc.util.Time;
 import discord4j.core.spec.EmbedCreateSpec;
-import mindustry.gen.Groups;
 import mindustry.net.Packets.KickReason;
 import pandorum.discord.BotHandler;
 import pandorum.discord.BotMain;
 
 import static mindustry.Vars.netServer;
-import static pandorum.Misc.bundled;
 
 public class RestartCommand {
     public static void run(final String[] args) {
-        boolean isCarefullyRestart = args.length > 0 && args[0] == "force" ? false : true;
+        Log.info("Сервер перезапускается...");
+        BotHandler.sendEmbed(EmbedCreateSpec.builder().color(BotMain.errorColor).title("Сервер выключается для перезапуска!").build());
 
-        BotHandler.sendEmbed(EmbedCreateSpec.builder().color(BotMain.errorColor).title("Сервер выключился для перезапуска!").build());
-
-        if (isCarefullyRestart) {
-            Log.err("Сервер будет перезапущен при отсутствии игроков...");
-            Groups.player.each(player -> bundled(player, "commands.restart.carefully"));
-
-            Timer.schedule(() -> {
-                if (Groups.player.size() != 0) return;
-                Log.err("Перезапуск сервера...");
-                System.exit(0);
-            }, 0, 1).run();
-        } else {
-            Log.err("Перезапуск сервера...");
-            netServer.kickAll(KickReason.serverRestarting);
-            System.exit(0);
-        }
+        netServer.kickAll(KickReason.serverRestarting);
+        Time.runTask(60f, () -> System.exit(2));
     }
 }
