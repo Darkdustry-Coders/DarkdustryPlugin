f78e0d32f4b32cb8085db179d926249d35231886
Даркнесс#3729 <79508138+Darkness6030@users.noreply.github.com>
2022-01-18 14:56:27 +0300

    Create SaveCommand.java

---
+++ src/main/java/pandorum/commands/server/SaveCommand.java
@@ -0,0 +1,25 @@
+package pandorum.commands.server;
+
+import arc.Core;
+import arc.files.Fi;
+import arc.util.Log;
+import arc.util.Strings;
+import mindustry.io.SaveIO;
+
+import static mindustry.Vars.saveDirectory;
+import static mindustry.Vars.saveExtension;
+import static mindustry.Vars.state;
+
+public class SaveCommand {
+    public static void run(final String[] args) {
+        if (state.isMenu()) {
+            Log.err("Сервер отключен. Может быть, пора запустить его командой 'host'?");
+            return;
+        }
+
+        Fi save = saveDirectory.child(Strings.format("@.@", args[0], saveExtension));
+
+        Core.app.post(() -> SaveIO.save(save));
+        Log.info("Карта сохранена в @", save);
+    }
+}
