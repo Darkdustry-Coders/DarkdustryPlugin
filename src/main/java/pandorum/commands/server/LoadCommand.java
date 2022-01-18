f7a88415321c0ba4a7b2215064336541b01848a1
Даркнесс#3729 <79508138+Darkness6030@users.noreply.github.com>
2022-01-18 19:24:32 +0300

    Create LoadCommand.java

---
+++ src/main/java/pandorum/commands/server/LoadCommand.java
@@ -0,0 +1,38 @@
+package pandorum.commands.server;
+
+import arc.Core;
+import arc.files.Fi;
+import arc.util.Log;
+import mindustry.io.SaveIO;
+import mindustry.io.SaveIO.SaveException;
+
+import static mindustry.Vars.*;
+import static pandorum.Misc.findSave;
+
+public class LoadCommand {
+    public static void run(final String[] args) {
+        if (!state.isMenu()) {
+            Log.err("Сервер уже запущен. Используй 'stop', чтобы остановить его.");
+            return;
+        }
+
+        Fi save = findSave(args[0]);
+        if (save == null) {
+            Log.err("Сохранение '@' не найдено.", args[0]);
+            return;
+        }
+
+        Core.app.post(() -> {
+            try {
+                SaveIO.load(save);
+                logic.play();
+
+                Log.info("Сохранение загружено.");
+
+                netServer.openServer();
+            } catch (SaveException e) {
+                Log.err(e);
+            }
+        });
+    }
+}
