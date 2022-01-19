38a27351c7d7ef8a325f31a7c270ed58af9c41ea
Даркнесс#3729 <79508138+Darkness6030@users.noreply.github.com>
2022-01-19 23:45:17 +0300

    Ладно

--- src/main/java/pandorum/events/listeners/TriggerUpdateListener.java
+++ src/main/java/pandorum/events/listeners/TriggerUpdateListener.java
@@ -2,25 +2,10 @@ package pandorum.events.listeners;
 
 import mindustry.gen.Groups;
 import pandorum.comp.Effects;
-import pandorum.comp.Ranks;
-import pandorum.models.PlayerModel;
-
-import static pandorum.PluginVars.interval;
 
 public class TriggerUpdateListener {
 
     public static void update() {
         Groups.player.each(player -> player.unit().moving(), p -> Effects.onMove(p.x, p.y));
-
-        if (interval.get(1, 60f)) {
-            Groups.player.each(player -> {
-                PlayerModel.find(player.uuid(), playerModel -> {
-                    playerModel.playTime += 1000L;
-                    playerModel.save();
-                });
-
-                Ranks.updateName(player);
-            });
-        }
     }
 }
