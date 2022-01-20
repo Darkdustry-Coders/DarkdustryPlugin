8520991bce936e4d02ecb2b26fac31ae21ff2ec0
Даркнесс#3729 <79508138+Darkness6030@users.noreply.github.com>
2022-01-20 12:34:47 +0300

    Update HelpCommand.java

--- src/main/java/pandorum/commands/client/HelpCommand.java
+++ src/main/java/pandorum/commands/client/HelpCommand.java
@@ -20,7 +20,7 @@ public class HelpCommand {
 
         Seq<Command> commandsList = CommandsHelper.getAvailableClientCommands(player.admin);
         int page = args.length > 0 ? Strings.parseInt(args[0]) : 1;
-        int pages = Mathf.ceil(commandsList.size / 6.0f);
+        int pages = Mathf.ceil(commandsList.size / 8f);
 
         if (--page >= pages || page < 0) {
             bundled(player, "commands.under-page", pages);
@@ -29,7 +29,7 @@ public class HelpCommand {
 
         StringBuilder result = new StringBuilder(Bundle.format("commands.help.page", findLocale(player.locale), page + 1, pages));
 
-        for (int i = 6 * page; i < Math.min(6 * (page + 1), commandsList.size); i++) {
+        for (int i = 8 * page; i < Math.min(8 * (page + 1), commandsList.size); i++) {
             Command command = commandsList.get(i);
             result.append("\n[orange] /").append(command.text).append("[white] ").append(command.paramText).append("[lightgray] - ").append(Bundle.get(Strings.format("commands.@.description", command.text), command.description, findLocale(player.locale)));
         }
