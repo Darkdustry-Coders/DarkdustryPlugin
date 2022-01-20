c9fd13b2e74c25255989df8f6b11a364c2b1909e
Даркнесс#3729 <79508138+Darkness6030@users.noreply.github.com>
2022-01-20 13:26:02 +0300

    Update SavesListCommand.java

--- src/main/java/pandorum/commands/client/SavesListCommand.java
+++ src/main/java/pandorum/commands/client/SavesListCommand.java
@@ -21,7 +21,7 @@ public class SavesListCommand {
 
         Seq<Fi> savesList = Seq.with(saveDirectory.list()).filter(file -> SaveIO.isSaveValid(file));
         int page = args.length > 0 ? Strings.parseInt(args[0]) : 1;
-        int pages = Mathf.ceil(savesList.size / 6.0f);
+        int pages = Mathf.ceil(savesList.size / 8f);
 
         if (--page >= pages || page < 0) {
             bundled(player, "commands.under-page", pages);
@@ -30,7 +30,7 @@ public class SavesListCommand {
 
         StringBuilder result = new StringBuilder(Bundle.format("commands.saves.page", findLocale(player.locale), page + 1, pages));
 
-        for (int i = 6 * page; i < Math.min(6 * (page + 1), savesList.size); i++) {
+        for (int i = 8 * page; i < Math.min(8 * (page + 1), savesList.size); i++) {
             Fi save = savesList.get(i);
             result.append("\n[lightgray] ").append(i + 1).append(". [orange]").append(save.nameWithoutExtension());
         }
