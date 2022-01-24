77e1d991b370dd85543464c3cc10968f23964017
Даркнесс#3729 <79508138+Darkness6030@users.noreply.github.com>
2022-01-24 22:28:06 +0300

    Update HubCommand.java

--- src/main/java/pandorum/commands/client/HubCommand.java
+++ src/main/java/pandorum/commands/client/HubCommand.java
@@ -11,6 +11,6 @@ import static pandorum.Misc.hubIp;
 public class HubCommand {
     public static void run(final String[] args, final Player player) {
         Tuple2<String, Integer> hub = hubIp();
-        net.pingHost(hub.t1, hub.t2, host -> Call.connect(player.con, hub.t1, hub.t2), e -> bundled(player, "commands.hub.offline"));
+        net.pingHost(hub.getT1(), hub.getT2(), host -> Call.connect(player.con, hub.getT1(), hub.getT2()), e -> bundled(player, "commands.hub.offline"));
     }
 }
