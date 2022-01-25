b25fed0267c57c3082580776649834f9ff26d48c
Даркнесс#3729 <79508138+Darkness6030@users.noreply.github.com>
2022-01-26 00:59:02 +0300

    Update NominateCommand.java

--- src/main/java/pandorum/commands/client/NominateCommand.java
+++ src/main/java/pandorum/commands/client/NominateCommand.java
@@ -15,18 +15,19 @@ import pandorum.vote.VoteSession;
 import static mindustry.Vars.saveDirectory;
 import static mindustry.Vars.saveExtension;
 import static pandorum.utils.Search.*;
+import static pandorum.utils.Utils.bundled;
 import static pandorum.PluginVars.*;
 
 public class NominateCommand {
     public static void run(final String[] args, final Player player) {
         if (currentVote[0] != null) {
-            Utils.bundled(player, "commands.vote-already-started");
+            bundled(player, "commands.vote-already-started");
             return;
         }
 
         Timekeeper vtime = nominateCooldowns.get(player.uuid(), () -> new Timekeeper(nominateCooldownTime));
         if (!vtime.get() && !player.admin) {
-            Utils.bundled(player, "commands.nominate.cooldown", Utils.secondsToMinutes(nominateCooldownTime));
+            bundled(player, "commands.nominate.cooldown", Utils.secondsToMinutes(nominateCooldownTime));
             return;
         }
 
@@ -34,7 +35,7 @@ public class NominateCommand {
             case "map" -> {
                 Map map = findMap(args[1]);
                 if (map == null) {
-                    Utils.bundled(player, "commands.nominate.map.not-found");
+                    bundled(player, "commands.nominate.map.not-found");
                     return;
                 }
                 VoteSession session = new VoteMapSession(currentVote, map);
@@ -44,10 +45,6 @@ public class NominateCommand {
             }
             case "save" -> {
                 Fi save = saveDirectory.child(Strings.format("@.@", args[1], saveExtension));
-                if (SaveIO.isSaveValid(save)) {
-                    Utils.bundled(player, "commands.nominate.save.already-exists");
-                    return;
-                }
                 VoteSession session = new VoteSaveSession(currentVote, save);
                 currentVote[0] = session;
                 session.vote(player, 1);
@@ -56,7 +53,7 @@ public class NominateCommand {
             case "load" -> {
                 Fi save = findSave(args[1]);
                 if (save == null) {
-                    Utils.bundled(player, "commands.nominate.load.not-found");
+                    bundled(player, "commands.nominate.load.not-found");
                     return;
                 }
                 VoteSession session = new VoteLoadSession(currentVote, save);
@@ -64,7 +61,7 @@ public class NominateCommand {
                 session.vote(player, 1);
                 vtime.reset();
             }
-            default -> Utils.bundled(player, "commands.nominate.incorrect-mode");
+            default -> bundled(player, "commands.nominate.incorrect-mode");
         }
     }
 }
