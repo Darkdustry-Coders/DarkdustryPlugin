b5c223854812d00eefdc76a9db0d9656e328ec53
Даркнесс#3729 <79508138+Darkness6030@users.noreply.github.com>
2022-01-25 19:09:36 +0300

    Update RestartCommand.java

--- src/main/java/pandorum/commands/server/RestartCommand.java
+++ src/main/java/pandorum/commands/server/RestartCommand.java
@@ -3,17 +3,12 @@ package pandorum.commands.server;
 import arc.util.Log;
 import arc.util.Time;
 import mindustry.net.Packets.KickReason;
-import net.dv8tion.jda.api.EmbedBuilder;
-
-import java.awt.*;
 
 import static mindustry.Vars.netServer;
-import static pandorum.discord.Bot.botChannel;
 
 public class RestartCommand {
     public static void run(final String[] args) {
         Log.info("Сервер перезапускается...");
-        botChannel.sendMessageEmbeds(new EmbedBuilder().setTitle("Сервер выключается для перезапуска!").setColor(Color.red).build()).queue();
 
         netServer.kickAll(KickReason.serverRestarting);
         Time.runTask(60f, () -> System.exit(2));
