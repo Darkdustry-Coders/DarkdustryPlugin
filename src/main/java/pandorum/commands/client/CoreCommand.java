7049771963c253e3d1db8bb2799011db9e72cc19
Даркнесс#3729 <79508138+Darkness6030@users.noreply.github.com>
2022-01-26 10:23:28 +0300

    Update CoreCommand.java

--- src/main/java/pandorum/commands/client/CoreCommand.java
+++ src/main/java/pandorum/commands/client/CoreCommand.java
@@ -9,23 +9,22 @@ import pandorum.comp.Icons;
 import pandorum.utils.Utils;
 
 import static pandorum.utils.Search.*;
-import static pandorum.utils.Utils.bundled;
 
 public class CoreCommand {
     public static void run(final String[] args, final Player player) {
         Block core = args.length > 0 ? findCore(args[0]) : Blocks.coreShard;
         if (core == null) {
-            bundled(player, "commands.admin.core.core-not-found");
+            Utils.bundled(player, "commands.admin.core.core-not-found");
             return;
         }
 
         Team team = args.length > 1 ? findTeam(args[1]) : player.team();
         if (team == null) {
-            bundled(player, "commands.team-not-found", Icons.teamsList());
+            Utils.bundled(player, "commands.team-not-found", Icons.teamsList());
             return;
         }
 
         Call.constructFinish(player.tileOn(), core, player.unit(), (byte) 0, team, false);
-        bundled(player, player.tileOn() != null && player.tileOn().block() == core ? "commands.admin.core.success" : "commands.admin.core.failed", Icons.get(core.name), Utils.colorizedTeam(team));
+        Utils.bundled(player, player.tileOn() != null && player.tileOn().block() == core ? "commands.admin.core.success" : "commands.admin.core.failed", Icons.get(core.name), Utils.colorizedTeam(team));
     }
 }
