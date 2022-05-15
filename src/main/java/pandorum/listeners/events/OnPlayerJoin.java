d33bd9a360d6dcc8b6e9c366ce25408606b1ff4c
Даркнесс <inflexibled@gmail.com>
2022-05-15 20:18:54 +0300

    проверка сохранения

--- src/main/java/pandorum/listeners/events/OnPlayerJoin.java
+++ src/main/java/pandorum/listeners/events/OnPlayerJoin.java
@@ -35,14 +35,7 @@ public class OnPlayerJoin implements Cons<PlayerJoin> {
 
         datas.put(event.player.uuid(), data);
 
-        // TEST
-        PlayerData test = new PlayerData();
-        test.rank = Ranks.admin;
-        test.buildingsBuilt = 1000 - 7;
-        test.playTime = -1;
-        setPlayerData(event.player.uuid(), test);
-
-        test = getPlayerData(event.player.uuid());
+        PlayerData test = getPlayerData(event.player.uuid());
         Log.info(test.rank);
         Log.info(test.buildingsBuilt);
         Log.info(test.playTime);
