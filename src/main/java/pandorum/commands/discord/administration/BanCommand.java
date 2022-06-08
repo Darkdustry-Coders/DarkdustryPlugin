14711ce42ae1223ccc68ecfc8ecd234bb30800a1
lucin <theeugenet@gmail.com>
2022-06-08 22:27:18 +0700

    Команды администрации в дискорде, ня. Потратил кучу времени на список игроков, когда Дарк это уже сделал. :(

---
+++ src/main/java/pandorum/commands/discord/administration/BanCommand.java
@@ -0,0 +1,54 @@
+package pandorum.commands.discord.administration;
+
+import arc.util.CommandHandler.CommandRunner;
+import arc.util.Log;
+import mindustry.gen.Groups;
+import mindustry.gen.Player;
+import pandorum.components.Bundle;
+import pandorum.discord.Context;
+import pandorum.util.Utils;
+
+import static mindustry.Vars.netServer;
+import static pandorum.PluginVars.discordServerUrl;
+import static pandorum.util.Search.findLocale;
+import static pandorum.util.Search.findPlayer;
+
+public class BanCommand implements CommandRunner<Context> {
+
+    @Override
+    public void accept(String[] args, Context context) {
+        switch (args[0].toLowerCase()) {
+            case "id", "uuid" -> {
+                netServer.admins.banPlayerID(args[1]);
+                Log.info("Игрок '@' успешно забанен.", args[1]);
+            }
+            case "name", "username" -> {
+                Player target = findPlayer(args[1]);
+                if (target != null) {
+                    netServer.admins.banPlayer(target.uuid());
+                    target.kick(Bundle.format("kick.banned", findLocale(target.locale), discordServerUrl), 0);
+                    Log.info("Игрок '@' успешно забанен.", target.name);
+                    Utils.sendToChat("events.server.ban", target.coloredName());
+                } else {
+                    Log.err("Игрок '@' не найден...", args[1]);
+                }
+                return;
+            }
+            case "ip", "address" -> {
+                netServer.admins.banPlayerIP(args[1]);
+                Log.info("Игрок '@' успешно забанен.", args[1]);
+            }
+            default -> {
+                context.err("Неверный тип целеуказания бана. Выбери один из этих: id, name, ip");
+                return;
+            }
+        }
+
+        Groups.player.each(player -> netServer.admins.isIDBanned(player.uuid()) || netServer.admins.isIPBanned(player.ip()), player -> {
+            player.kick(Bundle.format("kick.banned", findLocale(player.locale), discordServerUrl), 0);
+            Utils.sendToChat("events.server.ban", player.coloredName());
+        });
+
+        context.info(":skull: Игрок успешно забанен на сервере.");
+    }
+}
