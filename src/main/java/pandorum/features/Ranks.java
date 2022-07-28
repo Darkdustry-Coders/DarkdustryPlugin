eb08fb65846d5916eadaf5e548b83ca9e0428e97
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-07-28 15:47:28 +0300

    ooops...

--- src/main/java/pandorum/features/Ranks.java
+++ src/main/java/pandorum/features/Ranks.java
@@ -13,7 +13,6 @@ public class Ranks {
     // TODO упростить загрузку рангов?
     public static void load() {
         player = new Rank() {{
-            tag = "[accent]<[white]\uE800[]>[] ";
             name = "player";
             displayName = "[accent]Player";
 
