b1de329e0a982b3be2008b5ea6b438b4ed64e4eb
Даркнесс <inflexibled@gmail.com>
2022-08-23 19:27:19 +0300

    чета с командами

--- src/main/java/darkdustry/commands/DiscordCommands.java
+++ src/main/java/darkdustry/commands/DiscordCommands.java
@@ -4,7 +4,7 @@ import arc.Core;
 import arc.Events;
 import arc.files.Fi;
 import arc.func.Cons;
-import arc.struct.ObjectMap;
+import arc.struct.*;
 import arc.util.Time;
 import darkdustry.components.Config.Gamemode;
 import darkdustry.discord.Bot;
@@ -16,7 +16,8 @@ import mindustry.gen.Groups;
 import mindustry.net.Administration.Config;
 import mindustry.net.Packets.KickReason;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.requests.restaction.CommandCreateAction;
+import net.dv8tion.jda.api.interactions.commands.build.*;
+import net.dv8tion.jda.internal.interactions.CommandDataImpl;
 
 import java.awt.Color;
 
@@ -24,7 +25,6 @@ import static arc.Core.*;
 import static darkdustry.PluginVars.*;
 import static darkdustry.components.Bundle.*;
 import static darkdustry.components.MapParser.*;
-import static darkdustry.discord.Bot.*;
 import static darkdustry.utils.Checks.*;
 import static darkdustry.utils.Utils.*;
 import static mindustry.Vars.*;
@@ -38,7 +38,9 @@ public class DiscordCommands {
     public static final ObjectMap<String, Cons<SlashContext>> commands = new ObjectMap<>();
 
     public static void load() {
-        register("status", "Посмотреть статус сервера.", context -> {
+        var commands = new Seq<>();
+
+        commands.add(register("status", "Посмотреть статус сервера.", context -> {
             if (isMenu(context)) return;
 
             context.sendEmbed(new EmbedBuilder()
@@ -54,9 +56,9 @@ public class DiscordCommands {
                     .addField("До следующей волны:", formatDuration((int) state.wavetime / 60 * 1000L), true)
                     .setImage("attachment://minimap.png").build()
             ).addFiles(fromData(parseWorld(), "minimap.png").setDescription("Изображение мини-карты")).queue();
-        }).queue();
+        }));
 
-        register("players", "Список всех игроков на сервере.", PageIterator::players).addOption(INTEGER, "page", "Страница списка игроков.", false).queue();
+        register("players", "Список всех игроков на сервере.", PageIterator::players).addOption(INTEGER, "page", "Страница списка игроков.");
 
         register("kick", "Выгнать игрока с сервера.", context -> {
             if (notAdmin(context)) return;
@@ -66,9 +68,7 @@ public class DiscordCommands {
             kick(target, kickDuration, true, "kick.kicked");
             sendToChat("events.server.kick", target.name);
             context.info(":skull: Игрок успешно выгнан с сервера.", "@ не сможет зайти на сервер в течение @", target.name, formatDuration(kickDuration)).queue();
-        }).addOption(STRING, "name", "Имя игрока, которого нужно выгнать.", true)
-                .setDefaultPermissions(enabledFor(KICK_MEMBERS))
-                .queue();
+        }).addOption(STRING, "name", "Имя игрока, которого нужно выгнать.", true).setDefaultPermissions(enabledFor(KICK_MEMBERS));
 
         register("ban", "Забанить игрока на сервере.", context -> {
             if (notAdmin(context)) return;
@@ -79,9 +79,7 @@ public class DiscordCommands {
             kick(target, 0, true, "kick.banned");
             sendToChat("events.server.ban", target.name);
             context.info(":dagger: Игрок успешно забанен.", "@ больше не сможет зайти на сервер.", target.name).queue();
-        }).addOption(STRING, "name", "Имя игрока, которого нужно забанить.", true)
-                .setDefaultPermissions(enabledFor(BAN_MEMBERS))
-                .queue();
+        }).addOption(STRING, "name", "Имя игрока, которого нужно забанить.", true).setDefaultPermissions(enabledFor(BAN_MEMBERS));
 
         register("restart", "Перезапустить сервер.", context -> {
             if (notAdmin(context)) return;
@@ -92,7 +90,7 @@ public class DiscordCommands {
                 Core.app.post(Bot::exit);
                 Core.app.exit();
             });
-        }).setDefaultPermissions(enabledFor(ADMINISTRATOR)).queue();
+        }).setDefaultPermissions(enabledFor(ADMINISTRATOR));
 
         if (config.mode == Gamemode.hexed) return;
 
@@ -110,9 +108,9 @@ public class DiscordCommands {
                             .build()
                     ).addFiles(fromData(map.file.file()).setDescription("Файл карты"))
                     .addFiles(fromData(parseMap(map), "map.png").setDescription("Изображение карты")).queue();
-        }).addOption(STRING, "map", "Название карты, которую вы хотите получить.", true).queue();
+        }).addOption(STRING, "map", "Название карты, которую вы хотите получить.", true);
 
-        register("maps", "Список всех карт сервера.", PageIterator::maps).addOption(INTEGER, "page", "Страница списка карт.", false).queue();
+        register("maps", "Список всех карт сервера.", PageIterator::maps).addOption(INTEGER, "page", "Страница списка карт.");
 
         register("addmap", "Добавить карту на сервер.", context -> {
             if (notAdmin(context) || notMap(context)) return;
@@ -125,9 +123,7 @@ public class DiscordCommands {
                 maps.reload();
                 context.success(":map: Карта добавлена на сервер.").queue();
             });
-        }).addOption(ATTACHMENT, "map", "Файл карты, которую необходимо загрузить на сервер.", true)
-                .setDefaultPermissions(enabledFor(ADMINISTRATOR))
-                .queue();
+        }).addOption(ATTACHMENT, "map", "Файл карты, которую необходимо загрузить на сервер.", true).setDefaultPermissions(enabledFor(ADMINISTRATOR));
 
         register("removemap", "Удалить карту с сервера.", context -> {
             if (notAdmin(context)) return;
@@ -137,20 +133,22 @@ public class DiscordCommands {
             maps.removeMap(map);
             maps.reload();
             context.success(":dagger: Карта удалена с сервера.").queue();
-        }).addOption(STRING, "map", "Название карты, которую необходимо удалить с сервера.", true)
-                .setDefaultPermissions(enabledFor(ADMINISTRATOR))
-                .queue();
+        }).addOption(STRING, "map", "Название карты, которую необходимо удалить с сервера.", true).setDefaultPermissions(enabledFor(ADMINISTRATOR));
 
         register("gameover", "Принудительно завершить игру.", context -> {
             if (notAdmin(context) || isMenu(context)) return;
 
             Events.fire(new GameOverEvent(state.rules.waveTeam));
             context.success(":map: Игра успешно завершена.").queue();
-        }).setDefaultPermissions(enabledFor(ADMINISTRATOR)).queue();
+        }).setDefaultPermissions(enabledFor(ADMINISTRATOR));
+
+        commands.each(command -> {
+            // доделать
+        });
     }
 
-    public static CommandCreateAction register(String name, String description, Cons<SlashContext> cons) {
+    public static SlashCommandData register(String name, String description, Cons<SlashContext> cons) {
         commands.put(name, cons);
-        return botGuild.upsertCommand(name, description);
+        return new CommandDataImpl(name, description);
     }
 }
