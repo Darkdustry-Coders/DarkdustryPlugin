678341990e3745257df17903e5c80cf0ecefc5d6
EvDes190 <89368408+EvDes190@users.noreply.github.com>
2022-08-30 17:01:53 +0300

    Update Effects.java

--- src/main/java/darkdustry/features/Effects.java
+++ src/main/java/darkdustry/features/Effects.java
@@ -17,7 +17,7 @@ public class Effects {
 
     public static final ObjectMap<String, FxPack> cache = new ObjectMap<>();
 
-    public static FxPack pack1, pack2, pack3, pack4, pack5, pack6, pack7;
+    public static FxPack pack1, pack2, pack3, pack4, pack5, pack6, pack7, pack8;
 
     public static void load() {
         pack1 = new FxPack(p -> on(Fx.greenBomb, p), p -> on(Fx.greenLaserCharge, p), p -> on(Fx.freezing, p));
@@ -25,8 +25,9 @@ public class Effects {
         pack3 = new FxPack(p -> on(Fx.titanExplosion, p), p -> on(Fx.titanExplosion, p), p -> on(Fx.melting, p));
         pack4 = new FxPack(p -> on(Fx.scatheExplosion, p), p -> on(Fx.scatheExplosion, p), p -> on(Fx.electrified, p));
         pack5 = new FxPack(p -> on(Fx.instBomb, p), p -> on(Fx.instHit, p), p -> on(Fx.shootPayloadDriver, p, p.unit().rotation - 180f));
-        pack6 = new FxPack(p -> on(Fx.teleportActivate, p), p -> on(Fx.teleport, p), p -> on(Fx.smeltsmoke, p, 0f, Color.red));
-        pack7 = new FxPack(Effects::particles, Effects::particles, p -> on(Fx.regenSuppressSeek, p, 0f, Color.white, p.unit()));
+        pack6 = new FxPack(p -> on(Fx.teleportActivate, p), p -> on(Fx.teleport, p), p -> on(Fx.smeltsmoke, p, 0f, Color.scarlet));
+        pack7 = new FxPack(p -> on(Fx.teleportActivate, p), p -> on(Fx.teleport, p), p -> on(Fx.smeltsmoke, p, 0f, Color.#8d56b1));
+        pack8 = new FxPack(Effects::particles, Effects::particles, p -> on(Fx.regenSuppressSeek, p, 0f, Color.white, p.unit()));
     }
 
     public static void on(Effect effect, Position pos, float rotation, Color color, Object data) {
