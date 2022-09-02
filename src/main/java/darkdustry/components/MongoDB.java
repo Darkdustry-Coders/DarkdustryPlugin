f0c06ea88972e5b534e6fe3359185eaa5b58fb27
Даркнесс <inflexibled@gmail.com>
2022-09-02 20:56:07 +0300

    ну по идее так

--- src/main/java/darkdustry/components/MongoDB.java
+++ src/main/java/darkdustry/components/MongoDB.java
@@ -1,66 +1,38 @@
 package darkdustry.components;
 
-import arc.util.Log;
-import com.google.gson.Gson;
-import com.mongodb.ConnectionString;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.result.InsertOneResult;
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoClients;
-import com.mongodb.reactivestreams.client.MongoCollection;
-import com.mongodb.reactivestreams.client.MongoDatabase;
-import mindustry.gen.Player;
-import org.bson.BsonDocument;
-import org.bson.Document;
-import org.bson.codecs.configuration.CodecRegistry;
+import com.mongodb.reactivestreams.client.*;
+import darkdustry.DarkdustryPlugin;
+import darkdustry.utils.Utils;
 import org.bson.codecs.pojo.annotations.BsonProperty;
-import org.bson.conversions.Bson;
-import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
-
-import com.mongodb.*;
-import com.mongodb.reactivestreams.client.MongoClients;
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoCollection;
-import com.mongodb.reactivestreams.client.MongoDatabase;
-import com.mongodb.client.model.Projections;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Sorts;
-
-import java.util.Arrays;
-import org.bson.Document;
 import reactor.core.publisher.Mono;
 
-import static com.mongodb.client.model.Filters.*;
-import static com.mongodb.client.model.Projections.*;
-
-
+import static com.mongodb.client.model.Filters.eq;
 import static darkdustry.PluginVars.config;
 
 public class MongoDB {
-    public static MongoClient mongoClient;
-    public static MongoDatabase database;
+
     public static MongoCollection<PlayerData> collection;
 
     public static void connect() {
-        mongoClient = MongoClients.create(new ConnectionString(config.mongoConnectionString));
-        database = mongoClient.getDatabase("darkdustry");
-        collection = database.getCollection("playerData", PlayerData.class);
-    }
-    public static PlayerData getPlayerData(Player player) {
-        var data = collection.find(Filters.where("_id==="+player.uuid()));
-        var neko = Mono.from(data.first());
-        var pd = neko.block();
-        if(pd == null){
-            return new PlayerData(player.uuid());
+        try (var client = MongoClients.create(config.mongoConnectionString)) {
+            collection = client
+                    .getDatabase("darkdustry")
+                    .getCollection("playerData", PlayerData.class);
+
+            DarkdustryPlugin.info("Database connected.");
+        } catch (Exception e) {
+            DarkdustryPlugin.error("Failed to connect to the database: @", e);
         }
-        // todo
-        return pd;
     }
+
+    public static PlayerData getPlayerData(String uuid) {
+        return Utils.notNullElse(Mono.from(collection.find(eq(uuid)).first()).block(), new PlayerData(uuid));
+    }
+
     public static void setPlayerData(PlayerData data) {
         Mono.from(collection.insertOne(data)).subscribe();
     }
+
     public static class PlayerData {
         @BsonProperty("_id")
         public String uuid;
