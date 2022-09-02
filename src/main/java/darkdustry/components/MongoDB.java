c5707fdeecf8c84835e82a9746b6f1b0b9019507
Misaka <misakachan2545@gmail.com>
2022-09-02 20:34:02 +0300

    Больше асунк шызы для самошызения

--- src/main/java/darkdustry/components/MongoDB.java
+++ src/main/java/darkdustry/components/MongoDB.java
@@ -3,16 +3,37 @@ package darkdustry.components;
 import arc.util.Log;
 import com.google.gson.Gson;
 import com.mongodb.ConnectionString;
+import com.mongodb.client.model.Filters;
 import com.mongodb.client.result.InsertOneResult;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import com.mongodb.reactivestreams.client.MongoCollection;
 import com.mongodb.reactivestreams.client.MongoDatabase;
 import mindustry.gen.Player;
+import org.bson.BsonDocument;
 import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.conversions.Bson;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+
+import com.mongodb.*;
+import com.mongodb.reactivestreams.client.MongoClients;
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoCollection;
+import com.mongodb.reactivestreams.client.MongoDatabase;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Sorts;
+
+import java.util.Arrays;
+import org.bson.Document;
+import reactor.core.publisher.Mono;
+
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.*;
 
 
 import static darkdustry.PluginVars.config;
@@ -28,13 +49,20 @@ public class MongoDB {
         collection = database.getCollection("playerData", PlayerData.class);
     }
     public static PlayerData getPlayerData(Player player) {
+        var data = collection.find(Filters.where("_id==="+player.uuid()));
+        var neko = Mono.from(data.first());
+        var pd = neko.block();
+        if(pd == null){
+            return new PlayerData(player.uuid());
+        }
         // todo
-        return null;
+        return pd;
     }
     public static void setPlayerData(PlayerData data) {
-        collection.insertOne(data);
+        Mono.from(collection.insertOne(data)).subscribe();
     }
     public static class PlayerData {
+        @BsonProperty("_id")
         public String uuid;
         public String discord;
         public String language = "off";
