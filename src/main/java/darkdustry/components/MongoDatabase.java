aff7a804d58f3737d2623de2a76b08674b6a7bb8
Даркнесс <inflexibled@gmail.com>
2022-09-05 20:06:17 +0300

    Revert "фикс"
    This reverts commit dcf15e3cad9e13415bf3c9324bfa2275f6abd84a.

--- src/main/java/darkdustry/components/MongoDatabase.java
+++ src/main/java/darkdustry/components/MongoDatabase.java
@@ -6,9 +6,6 @@ import darkdustry.DarkdustryPlugin;
 import darkdustry.components.Database.PlayerData;
 import darkdustry.utils.Utils;
 import org.bson.Document;
-import org.bson.codecs.configuration.CodecProvider;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
 
 import static com.mongodb.MongoClientSettings.getDefaultCodecRegistry;
 import static com.mongodb.client.model.Filters.eq;
@@ -21,38 +18,39 @@ public class MongoDatabase {
     public static MongoCollection<PlayerData> collection;
 
     public static void load() {
-        try {
-            CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
-            CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
-            MongoClient client = MongoClients.create(config.mongoUrl);
-            com.mongodb.client.MongoDatabase db = client.getDatabase("darkdustry").withCodecRegistry(pojoCodecRegistry);
-            collection = db.getCollection("playerData", PlayerData.class);
+        try (var client = MongoClients.create(config.mongoUrl)) {
+            collection = client.getDatabase("darkdustry")
+                    .withCodecRegistry(fromRegistries(getDefaultCodecRegistry(), fromProviders(builder().automatic(true).build())))
+                    .getCollection("players", PlayerData.class);
             DarkdustryPlugin.info("Database connected.");
         } catch (Exception e) {
             DarkdustryPlugin.error("Failed to connect to the database: @", e);
         }
-        PlayerData data = getPlayerData("ejdndhjdbhdbhcbdcbd");
-        data.language = "ru_RU";
-        data.playTime = 100000000;
-        data.alertsEnabled = false;
+
+        PlayerData data = new PlayerData("hentai");
+        data.rank = 1000 - 7;
         setPlayerData(data);
+
+        PlayerData test = getPlayerData(data.uuid);
+        Log.info("Test: @", test.rank);
     }
 
     public static PlayerData getPlayerData(String uuid) {
-        var data = collection.find(eq("uuid", uuid)).first();
+        var data = collection.find(new Document("uuid", uuid)).first();
         return Utils.notNullElse(data, new PlayerData(uuid));
     }
 
     public static void setPlayerData(PlayerData data) {
         if (!hasPlayerData(data.uuid)) {
             collection.insertOne(data);
-        } else {
-            collection.replaceOne(eq("uuid", data.uuid), data);
+            return;
         }
+
+        collection.replaceOne(eq("uuid", data.uuid), data);
     }
 
     public static boolean hasPlayerData(String uuid) {
-        var data = collection.find(eq("uuid", uuid));
+        var data = collection.find(eq("uuid", uuid)).first();
         return data != null;
     }
 }
