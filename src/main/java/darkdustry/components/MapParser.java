d509cf04fcf76ae0083b7e214a80a1c1e7c59a45
Даркнесс <inflexibled@gmail.com>
2022-09-10 16:14:00 +0300

    УБЕЙТЕ МЕНЯ

--- src/main/java/darkdustry/components/MapParser.java
+++ src/main/java/darkdustry/components/MapParser.java
@@ -2,9 +2,10 @@ package darkdustry.components;
 
 import arc.graphics.*;
 import arc.graphics.PixmapIO.PngWriter;
-import arc.util.io.CounterInputStream;
+import arc.util.io.*;
 import darkdustry.DarkdustryPlugin;
 import mindustry.content.Blocks;
+import mindustry.ctype.*;
 import mindustry.game.Team;
 import mindustry.io.*;
 import mindustry.maps.Map;
@@ -47,6 +48,10 @@ public class MapParser {
         }
     }
 
+    public static byte[] renderMinimap() {
+        return parseImage(MapIO.generatePreview(world.tiles));
+    }
+
     public static Pixmap generatePreview(Map map) throws IOException {
         try (CounterInputStream counter = new CounterInputStream(new InflaterInputStream(map.file.read(bufferSize))); DataInputStream stream = new DataInputStream(counter)) {
             SaveIO.readHeader(stream);
@@ -57,7 +62,7 @@ public class MapParser {
             Pixmap walls = new Pixmap(map.width, map.height);
             int black = 255;
             int shade = Color.rgba8888(0f, 0f, 0f, 0.5f);
-            CachedTile tile = new CachedTile() {
+            CachedTile cachedTile = new CachedTile() {
                 @Override
                 public void setBlock(Block type) {
                     super.setBlock(type);
@@ -70,10 +75,21 @@ public class MapParser {
                 }
             };
 
-            version.region("content", stream, counter, version::readContentHeader);
-            content.setTemporaryMapper(null);
+            var contents = new MappableContent[ContentType.all.length][0];
 
-            version.region("preview_map", stream, counter, in -> version.readMap(in, new WorldContext() {
+            version.region("content", stream, counter, input -> {
+                for (int i = 0; i < stream.readByte(); i++) {
+                    var type = ContentType.all[stream.readByte()];
+                    contents[type.ordinal()] = new MappableContent[stream.readShort()];
+
+                    for (int j = 0; j < contents[type.ordinal()].length; j++) {
+                        String name = stream.readUTF();
+                        contents[type.ordinal()][j] = content.getByName(type, SaveFileReader.fallback.get(name, name));
+                    }
+                }
+            });
+
+            var context = new WorldContext() {
                 @Override
                 public void resize(int width, int height) {}
 
@@ -90,15 +106,14 @@ public class MapParser {
 
                 @Override
                 public void onReadBuilding() {
-                    if (tile.build != null) {
-                        int size = tile.block().size;
+                    if (cachedTile.build != null) {
+                        int size = cachedTile.block().size;
                         int offsetx = -(size - 1) / 2;
                         int offsety = -(size - 1) / 2;
-
-                        for (int x = 0; x < size; x++) {
-                            for (int y = 0; y < size; y++) {
-                                int drawx = tile.x + x + offsetx, drawy = tile.y + y + offsety;
-                                walls.set(drawx, floors.height - 1 - drawy, tile.build.team.color.rgba8888());
+                        for (int dx = 0; dx < size; dx++) {
+                            for (int dy = 0; dy < size; dy++) {
+                                int drawx = cachedTile.x + dx + offsetx, drawy = cachedTile.y + dy + offsety;
+                                walls.set(drawx, floors.height - 1 - drawy, cachedTile.build.team.color.rgba8888());
                             }
                         }
                     }
@@ -106,9 +121,9 @@ public class MapParser {
 
                 @Override
                 public Tile tile(int index) {
-                    tile.x = (short) (index % map.width);
-                    tile.y = (short) (index / map.width);
-                    return tile;
+                    cachedTile.x = (short) (index % map.width);
+                    cachedTile.y = (short) (index / map.width);
+                    return cachedTile;
                 }
 
                 @Override
@@ -118,20 +133,81 @@ public class MapParser {
                     } else {
                         floors.set(x, floors.height - 1 - y, colorFor(Blocks.air, content.block(floorID), Blocks.air, Team.derelict));
                     }
-                    return tile;
+                    return cachedTile;
                 }
-            }));
+            };
+
+            version.region("preview_map", stream, counter, input -> {
+                int width = stream.readUnsignedShort();
+                int height = stream.readUnsignedShort();
+
+                context.resize(width, height);
+
+                for (int i = 0; i < width * height; i++) {
+                    int x = i % width, y = i / width;
+                    short floorid = stream.readShort();
+                    short oreid = stream.readShort();
+                    int consecutives = stream.readUnsignedByte();
+
+                    context.create(x, y, floorid, oreid, (short) 0);
+
+                    for (int j = i + 1; j < i + 1 + consecutives; j++) {
+                        int newx = j % width, newy = j / width;
+                        context.create(newx, newy, floorid, oreid, (short) 0);
+                    }
+
+                    i += consecutives;
+                }
+
+                for (int i = 0; i < width * height; i++) {
+                    Block block = (Block) contents[ContentType.block.ordinal()][(stream.readShort())];
+                    Tile tile = context.tile(i);
+                    if (block == null) block = Blocks.air;
+                    boolean isCenter = true;
+                    byte packedCheck = stream.readByte();
+                    boolean hadEntity = (packedCheck & 1) != 0;
+                    boolean hadData = (packedCheck & 2) != 0;
+
+                    if (hadEntity) {
+                        isCenter = stream.readBoolean();
+                    }
+
+                    if (isCenter) {
+                        tile.setBlock(block);
+                    }
+
+                    if (hadEntity) {
+                        if (isCenter) {
+                            if (block.hasBuilding()) {
+                                version.readChunk(stream, true, in -> {
+                                    byte revision = in.readByte();
+                                    tile.build.readAll(Reads.get(in), revision);
+                                });
+                            } else {
+                                version.skipChunk(input);
+                            }
+
+                            context.onReadBuilding();
+                        }
+                    } else if (hadData) {
+                        tile.setBlock(block);
+                        tile.data = stream.readByte();
+                    } else {
+                        int consecutives = stream.readUnsignedByte();
+
+                        for (int j = i + 1; j < i + 1 + consecutives; j++) {
+                            context.tile(j).setBlock(block);
+                        }
+
+                        i += consecutives;
+                    }
+                }
+            });
 
             floors.draw(walls, true);
             walls.dispose();
             return floors;
-        } finally {
-            content.setTemporaryMapper(null);
         }
-    }
-
-    public static byte[] renderMinimap() {
-        return parseImage(MapIO.generatePreview(world.tiles));
     }
 
     public static byte[] parseImage(Pixmap pixmap) {
