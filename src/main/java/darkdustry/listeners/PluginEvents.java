3ef29040aed1bba2ae703275cb80eb47421d6730
Даркнесс#3729 <79508138+Darkness6030@users.noreply.github.com>
2022-11-17 15:37:02 +0300

    А ну да

--- src/main/java/darkdustry/listeners/PluginEvents.java
+++ src/main/java/darkdustry/listeners/PluginEvents.java
@@ -37,10 +37,8 @@ public class PluginEvents {
 
             state.rules.revealedBlocks.addAll(Blocks.slagCentrifuge, Blocks.heatReactor, Blocks.scrapWall, Blocks.scrapWallLarge, Blocks.scrapWallHuge, Blocks.scrapWallGigantic, Blocks.thruster);
 
-            if (config.mode == sandbox) {
-                state.rules.bannedBlocks.addAll(Blocks.microProcessor, Blocks.logicProcessor, Blocks.hyperProcessor);
+            if (config.mode == sandbox)
                 state.rules.revealedBlocks.addAll(Blocks.shieldProjector, Blocks.largeShieldProjector, Blocks.beamLink);
-            }
         });
 
         Events.on(GameOverEvent.class, event -> getPlayersData(Groups.player).doOnNext(data -> data.gamesPlayed++).flatMap(Database::setPlayerData).subscribe());
