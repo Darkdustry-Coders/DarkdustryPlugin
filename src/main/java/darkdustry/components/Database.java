b9b18326075251a3a4f219d6f26346e80870e90a
Даркнесс#3729 <79508138+Darkness6030@users.noreply.github.com>
2023-03-27 08:19:48 +0300

    Update Database.java

--- src/main/java/darkdustry/components/Database.java
+++ src/main/java/darkdustry/components/Database.java
@@ -1,8 +1,6 @@
 package darkdustry.components;
 
 import arc.func.*;
-import arc.struct.ObjectMap;
-import arc.util.Log;
 import arc.util.serialization.*;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.model.ReplaceOptions;
@@ -12,7 +10,6 @@ import darkdustry.DarkdustryPlugin;
 import darkdustry.features.Ranks.Rank;
 import darkdustry.features.menus.MenuHandler.*;
 import mindustry.gen.Player;
-import mindustry.io.JsonIO;
 import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.pojo.PojoCodecProvider;
 import reactor.core.publisher.*;
@@ -34,35 +31,6 @@ public class Database {
             database = client.getDatabase("darkdustry").withCodecRegistry(CodecRegistries.fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), CodecRegistries.fromProviders(PojoCodecProvider.builder().automatic(true).build())));
 
             playersCollection = database.getCollection("players", PlayerData.class);
-
-            class PlayerData implements Json.JsonSerializable {
-                public String name;
-                public int wins;
-
-                @Override
-                public void write(Json json) {
-                    json.writeValue("name", name);
-                    json.writeValue("wins", wins);
-                }
-
-                @Override
-                public void read(Json json, JsonValue value) {
-                    name = value.getString("name");
-                    wins = value.getInt("wins");
-                }
-            }
-
-            ObjectMap<String, PlayerData> map = JsonIO.json.fromJson(ObjectMap.class, PlayerData.class, dataDirectory.child("database.json"));
-
-            Flux.fromIterable(new ObjectMap.Entries<>(map))
-                    .filter(entry -> entry.value.wins > 0)
-                    .flatMap(entry -> {
-                        return getPlayerData(entry.key).zipWith(Mono.just(entry.value));
-                    }).flatMap(tuple -> {
-                        Log.info(tuple.getT1().name);
-                        tuple.getT1().hexedWins = tuple.getT2().wins;
-                        return setPlayerData(tuple.getT1());
-                    }).subscribe();
 
             DarkdustryPlugin.info("Database connected.");
         } catch (Exception e) {
