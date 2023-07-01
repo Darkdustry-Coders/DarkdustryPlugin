f92cadee8f7a77a2f3792fb8848b6f62dd16195a
Darkness <inflexibled@gmail.com>
2023-07-01 12:14:27 +0300

    этим никто не пользуется

--- src/main/java/darkdustry/utils/Utils.java
+++ src/main/java/darkdustry/utils/Utils.java
@@ -26,8 +26,8 @@ public class Utils {
 
     public static int voteChoice(String vote) {
         return switch (stripFooCharacters(vote.toLowerCase())) {
-            case "y", "yes" -> 1;
-            case "n", "no" -> -1;
+            case "y" -> 1;
+            case "n" -> -1;
             default -> 0;
         };
     }
