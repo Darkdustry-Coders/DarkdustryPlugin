7b418a78190d3c4a3d91ffb64e1eb34c65b4d977
Darkness <inflexibled@gmail.com>
2023-11-06 17:37:43 +0300

    а нахуя два раза загружать

--- src/main/java/darkdustry/DarkdustryPlugin.java
+++ src/main/java/darkdustry/DarkdustryPlugin.java
@@ -39,7 +39,6 @@ public class DarkdustryPlugin extends Plugin {
         Commands.load();
 
         Alerts.load();
-        Commands.load();
         MenuHandler.load();
         SchemeSize.load();
 
