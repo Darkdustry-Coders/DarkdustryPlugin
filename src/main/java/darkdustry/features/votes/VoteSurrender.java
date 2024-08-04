46c61e7219a813514d1051af6d70e012f9591023
buj <buj@localhost>
2024-08-04 20:40:19 +0700

    /surrender: don't destroy world logic

--- src/main/java/darkdustry/features/votes/VoteSurrender.java
+++ src/main/java/darkdustry/features/votes/VoteSurrender.java
@@ -1,9 +1,15 @@
 package darkdustry.features.votes;
 
 import arc.math.Mathf;
+import arc.struct.Seq;
+import arc.util.Time;
+import mindustry.content.Blocks;
 import mindustry.game.Team;
 import mindustry.gen.*;
+import mindustry.world.blocks.storage.CoreBlock;
 import useful.Bundle;
+
+import java.util.Objects;
 
 import static darkdustry.PluginVars.*;
 import static darkdustry.utils.Checks.*;
@@ -34,7 +40,31 @@ public class VoteSurrender extends VoteSession{
         stop();
         Bundle.send("commands.surrender.success", team.coloredName());
 
-        team.data().destroyToDerelict();
+        var data = team.data();
+
+        var builds = new Seq<Building>();
+        if (data.buildingTree != null) {
+            data.buildingTree.getObjects(builds);
+        }
+
+        data.plans.clear();
+
+        for (Building b : builds) {
+            if (b instanceof CoreBlock.CoreBuild) {
+                b.kill();
+            } else if (b.block != Blocks.worldCell && b.block != Blocks.worldProcessor) {
+                Call.setTeam(b, Team.derelict);
+                if (Mathf.chance(0.25)) {
+                    Time.run(Mathf.random(0.0F, 360.0F), b::kill);
+                }
+            }
+        }
+
+        data.units.each((u) -> {
+            Time.run(Mathf.random(0.0F, 300.0F), () -> {
+                if (u.team == this.team) u.kill();
+            });
+        });
     }
 
     @Override
